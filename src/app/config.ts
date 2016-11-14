d3431403faada7bfe03f315f7d5c073dcd1153c9
Kim Rutherford <kmr44@cam.ac.uk>
2016-11-14 23:25:54 +1300

    Fix indentation

--- src/app/config.ts
+++ src/app/config.ts
@@ -12,47 +12,47 @@ export interface GenePageConfig {
 }
 
 let _config: GenePageConfig = {
-    annotationTypes: {
-        molecular_function: {
-            displayName: "GO molecular function",
-        },
-        biological_process: {
-            displayName: "GO biological process",
-        },
-        cellular_component: {
-            displayName: "GO cellular component",
-        },
-        "PSI-MOD": {
-            displayName: "Modification",
-        },
-        fission_yeast_phenotype: {
-            displayName: "Phenotype",
-        },
-        post_translational_modification: {
-            displayName: "Protein modification",
-        },
-        genetic_interaction: {
-            displayName: "Genetic interaction",
-        },
-        physical_interaction: {
-            displayName: "Physical interaction",
-        },
-        gene_ex: {
-            displayName: "Gene expression",
-        },
-        species_dist: {
-            displayName: "Taxonomic conservation"
-        },
-        complementation: {
-            displayName: "Complementation"
-        }
+  annotationTypes: {
+    molecular_function: {
+      displayName: "GO molecular function",
     },
-    getAnnotationType:
-    function(annotationTypeName: string): AnnotationType {
-        return _config.annotationTypes[annotationTypeName];
+    biological_process: {
+      displayName: "GO biological process",
     },
+    cellular_component: {
+      displayName: "GO cellular component",
+    },
+    "PSI-MOD": {
+      displayName: "Modification",
+    },
+    fission_yeast_phenotype: {
+      displayName: "Phenotype",
+    },
+    post_translational_modification: {
+      displayName: "Protein modification",
+    },
+    genetic_interaction: {
+      displayName: "Genetic interaction",
+    },
+    physical_interaction: {
+      displayName: "Physical interaction",
+    },
+    gene_ex: {
+      displayName: "Gene expression",
+    },
+    species_dist: {
+      displayName: "Taxonomic conservation"
+    },
+    complementation: {
+      displayName: "Complementation"
+    },
+  },
+  getAnnotationType:
+  function(annotationTypeName: string): AnnotationType {
+    return _config.annotationTypes[annotationTypeName];
+  },
 }
 
 export function getGenePageConfig(): GenePageConfig {
-    return _config;
+  return _config;
 }
