397d30ec614879f3ab2ea9f2b2d6bb4c49bd8d26
Kim Rutherford <kmr44@cam.ac.uk>
2016-11-14 19:42:02 +1300

    Configure display names for all CVs

--- src/app/config.ts
+++ src/app/config.ts
@@ -13,8 +13,38 @@ export interface GenePageConfig {
 
 let _config: GenePageConfig = {
     annotationTypes: {
+        molecular_function: {
+            displayName: "GO molecular function",
+        },
+        biological_process: {
+            displayName: "GO biological process",
+        },
         cellular_component: {
             displayName: "GO cellular component",
+        },
+        "PSI-MOD": {
+            displayName: "Modification",
+        },
+        fission_yeast_phenotype: {
+            displayName: "Phenotype",
+        },
+        post_translational_modification: {
+            displayName: "Protein modification",
+        },
+        genetic_interaction: {
+            displayName: "Genetic interaction",
+        },
+        physical_interaction: {
+            displayName: "Physical interaction",
+        },
+        gene_ex: {
+            displayName: "Gene expression",
+        },
+        species_dist: {
+            displayName: "Taxonomic conservation"
+        },
+        complementation: {
+            displayName: "Complementation"
         }
     },
     getAnnotationType:
