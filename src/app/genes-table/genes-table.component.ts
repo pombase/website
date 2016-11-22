31e352b294cdce8e17beade8a9073d8bbea6a7be
Kim Rutherford <kmr44@cam.ac.uk>
2016-11-23 12:31:46 +1300

    Show the spinner while loading the term gene list

--- src/app/genes-table/genes-table.component.ts
+++ src/app/genes-table/genes-table.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class GenesTableComponent {
   @Input() legend: string;
-  @Input() genes: Array<any>;
+  @Input() genes: Array<any> = [];
 
   constructor() { }
 }
