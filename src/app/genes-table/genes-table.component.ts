5f59f1d81fc400eb06d77abd8d17a6676b345112
Kim Rutherford <kmr44@cam.ac.uk>
2016-11-23 12:29:47 +1300

    Remove gene page pagination for now

--- src/app/genes-table/genes-table.component.ts
+++ src/app/genes-table/genes-table.component.ts
@@ -1,34 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-genes-table',
   templateUrl: './genes-table.component.html',
   styleUrls: ['./genes-table.component.css']
 })
-export class GenesTableComponent implements OnInit {
+export class GenesTableComponent {
   @Input() legend: string;
   @Input() genes: Array<any>;
 
-  public currentPage: number = 1;
-  public itemsPerPage: number = 20;
-  public showPagination = true;
-  public thisPageGenes: Array<any>;
-  
   constructor() { }
-
-  ngOnInit() {
-    this.onPageChange({page: this.currentPage, itemsPerPage: this.itemsPerPage});
-  }
-
-  showAll() {
-    this.itemsPerPage = this.genes.length;
-    this.showPagination = false;
-    this.onPageChange({page: 1, itemsPerPage: this.itemsPerPage});
-  }
-
-  onPageChange(page: any) {
-    let start = (page.page - 1) * page.itemsPerPage;
-    let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : this.genes.length;
-    this.thisPageGenes = this.genes.slice(start, end);
-  }
 }
