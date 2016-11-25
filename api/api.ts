55b84d90e33943dfed6de0e6d8cca2e801be195a
Kim Rutherford <kmr44@cam.ac.uk>
2016-11-25 19:14:00 +1300

    Load search_api_maps.json and add test routes

--- api/api.ts
+++ api/api.ts
@@ -1,7 +1,9 @@
 import * as bodyParser from "body-parser";
 import * as express from "express";
+import {Request, Response} from "express";
 import * as path from "path";
 
+import fs = require('fs');
 
 if (process.argv.length != 3) {
   console.log("needs one argument");
@@ -10,15 +12,23 @@ if (process.argv.length != 3) {
 
 let directory = process.argv[2];
 
+let searchMapsJSON = fs.readFileSync(directory + "/search_api_maps.json", "utf8");
+let searchMaps = JSON.parse(searchMapsJSON);
 
-var app = express();
+let app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var port = normalizePort(process.env.POMBASE_API_PORT || '4001');
+let port = normalizePort(process.env.POMBASE_API_PORT || '4001');
 
-var routes = require('./routes/index');
+// pass maps to route handlers
+app.use(function(req: Request, res: Response, next: Function) {
+  res.locals.searchMaps = searchMaps;
+  next();
+});
+
+let routes = require('./routes/index');
 app.use('/', routes);
 
 app.listen(port);
@@ -26,7 +36,7 @@ console.log("listening on port: " + port);
 
 
 function normalizePort(val: string): string | number | boolean {
-  var port = parseInt(val, 10);
+  let port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
--- api/routes/index.ts
+++ api/routes/index.ts
@@ -1,9 +1,28 @@
 import {Request, Response} from "express";
-var express = require('express');
-var router = express.Router();
 
-router.get('/v1/gene/:uniquename', function(req: Request, res: Response, next: Function) {
-  res.json({ message: 'hooray! welcome to our api!' + req.params['uniquename'] });
+let express = require('express');
+let router = express.Router();
+
+
+router.get('/gene/by_termid/:termid', function(req: Request, res: Response, next: Function) {
+  let searchMaps = res.locals.searchMaps;
+  res.json({ matches: searchMaps.termid_genes[req.params['termid']] });
+});
+
+router.get('/gene/by_term_name_exact/:termName', function(req: Request, res: Response, next: Function) {
+  let searchMaps = res.locals.searchMaps;
+  res.json({ matches: searchMaps.term_name_genes[req.params['termName']] });
+});
+
+router.get('/gene/by_term_name_fuzzy/:termName', function(req: Request, res: Response, next: Function) {
+  let searchMaps = res.locals.searchMaps;
+  let matches: any = { };
+  for (let thisTermName of Object.keys(searchMaps.term_name_genes)) {
+    if (thisTermName.includes(req.params['termName'])) {
+      matches[thisTermName] = searchMaps.term_name_genes[thisTermName];
+    }
+  }
+  res.json({ matches: matches });
 });
 
 module.exports = router;
