51fac9f6ce0343f6e816a25d133fee87d7398367
Kim Rutherford <kmr44@cam.ac.uk>
2016-11-29 16:47:27 +1300

    Fix missing mocha import in test

--- api/tests/test_query.ts
+++ api/tests/test_query.ts
@@ -1,10 +1,13 @@
+import 'mocha';
+
 import { QueryHandler } from '../query';
+
 var assert = require('assert');
 
 describe('QueryHandler', function() {
   describe('#getGenesOfPart()', function() {
     it('should return no matches', function() {
-      var qh = new QueryHandler('./test/data/');
+      var qh = new QueryHandler('./tests/data');
       assert.equal([], []);
     });
   });
