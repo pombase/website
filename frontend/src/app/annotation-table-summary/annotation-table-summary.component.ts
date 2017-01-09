d149b8eb40f6ef09b9e9b672d223f6a3c074a98f
Kim Rutherford <kmr44@cam.ac.uk>
2017-01-09 21:16:19 +1300

    Remove unused imports

--- frontend/src/app/annotation-table-summary/annotation-table-summary.component.ts
+++ frontend/src/app/annotation-table-summary/annotation-table-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { TermAnnotation } from '../pombase-api.service';
 
 import { getAnnotationTableConfig, AnnotationTableConfig } from '../config';
