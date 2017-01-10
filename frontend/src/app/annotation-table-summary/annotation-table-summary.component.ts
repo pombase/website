aae7735e79a87378601b649b6201e9724af82f79
Kim Rutherford <kmr44@cam.ac.uk>
2017-01-10 16:04:44 +1300

    Make sure to update the extension on changes
    Fixes #120

--- frontend/src/app/annotation-table-summary/annotation-table-summary.component.ts
+++ frontend/src/app/annotation-table-summary/annotation-table-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { TermAnnotation } from '../pombase-api.service';
 
 import { getAnnotationTableConfig, AnnotationTableConfig } from '../config';
@@ -8,7 +8,7 @@ import { getAnnotationTableConfig, AnnotationTableConfig } from '../config';
   templateUrl: './annotation-table-summary.component.html',
   styleUrls: ['./annotation-table-summary.component.css']
 })
-export class AnnotationTableSummaryComponent implements OnInit {
+export class AnnotationTableSummaryComponent implements OnInit, OnChanges {
   @Input() annotationTable: Array<TermAnnotation>;
 
   annotationTypeDisplayName = null;
@@ -86,20 +86,26 @@ export class AnnotationTableSummaryComponent implements OnInit {
   }
 
   makeExtensionSummaries() {
-    for (let term_annotation of this.annotationTable) {
-      let termid = term_annotation.term.termid;
-      let thisTermExtensions =
-        term_annotation.annotations.map(annotation => annotation.extension)
-        .filter(extension => extension && extension.length > 0);
-      if (thisTermExtensions.length > 0) {
-        this.extensionSummariesByTerm[termid] = this.compactExtensions(thisTermExtensions);
+    this.extensionSummariesByTerm = {};
+
+    if (this.annotationTable) {
+      for (let term_annotation of this.annotationTable) {
+        let termid = term_annotation.term.termid;
+        let thisTermExtensions =
+          term_annotation.annotations.map(annotation => annotation.extension)
+          .filter(extension => extension && extension.length > 0);
+        if (thisTermExtensions.length > 0) {
+          this.extensionSummariesByTerm[termid] = this.compactExtensions(thisTermExtensions);
+        }
       }
     }
   }
 
+  ngOnChanges() {
+    this.makeExtensionSummaries();
+  }
+
   ngOnInit() {
-    if (this.annotationTable) {
-      this.makeExtensionSummaries();
-    }
+    this.makeExtensionSummaries();
   }
 }
