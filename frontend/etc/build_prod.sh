d72f3a8f99f8644ed2cf3a7a529435a8bccfd4f1
Kim Rutherford <kmr44@cam.ac.uk>
2017-02-15 19:49:41 +1300

    Include Ensembl link from GO.xrf_abbs
    Refs #165

--- frontend/etc/build_prod.sh
+++ frontend/etc/build_prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 
-ABBREVS="SO SGD PR RGD MGI EC Pfam PANTHER UniProtKB UniProtKB-SubCell UniPathway FB InterPro ECOGENE UniProtKB-KW PDB WB AGI_LocusCode dictyBase"
+ABBREVS="Ensembl SO SGD PR RGD MGI EC Pfam PANTHER UniProtKB UniProtKB-SubCell UniPathway FB InterPro ECOGENE UniProtKB-KW PDB WB AGI_LocusCode dictyBase"
 
 etc/make-link-js.pl /var/pomcur/sources/go-svn/doc/GO.xrf_abbs $ABBREVS > src/app/config/external-links.json
 ng build --env=prod --target=production && (cd dist; rsync -cavP * oliver0:/var/www/www-pombase2/)
