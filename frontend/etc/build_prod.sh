8b1c70c22cc3a8bf2362bde317c97f2f3ccc918c
Kim Rutherford <kmr44@cam.ac.uk>
2017-04-05 11:05:42 +1200

    Add ModBase to external link config

--- frontend/etc/build_prod.sh
+++ frontend/etc/build_prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -
 
-ABBREVS="ENSEMBL SO SGD PR RGD MGI EC Pfam PANTHER UniProtKB UniProtKB-SubCell UniPathway FB InterPro ECOGENE UniProtKB-KW PDB WB AGI_LocusCode dictyBase"
+ABBREVS="ENSEMBL SO SGD PR RGD MGI EC Pfam PANTHER UniProtKB UniProtKB-SubCell UniPathway FB InterPro ECOGENE UniProtKB-KW PDB WB AGI_LocusCode dictyBase ModBase"
 
 etc/make-link-js.pl /var/pomcur/sources/go-svn/doc/GO.xrf_abbs $ABBREVS > src/app/config/go-xrf-abbr-external-links.json
 ng build --env=prod --target=production && (cd dist; rsync -cavP * oliver0:/var/www/www-pombase2/)
