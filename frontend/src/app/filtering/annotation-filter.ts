4d55a7fe330d631cb62045671d29aed549d6cc59
Kim Rutherford <kmr44@cam.ac.uk>
2017-04-10 17:57:30 +1200

    Remove non-existent import

--- frontend/src/app/filtering/annotation-filter.ts
+++ frontend/src/app/filtering/annotation-filter.ts
@@ -1,6 +1,5 @@
 import { AnnotationTable } from '../pombase-api.service';
-import { TermSummary } from '../pombase-api.service';
 
 export interface AnnotationFilter {
-  filter(annotationTable: AnnotationTable|Array<TermSummary>): AnnotationTable|Array<TermSummary>;
+  filter(annotationTable: AnnotationTable): AnnotationTable;
 }
