9754084d962007ef61d4b9e1c21afef1f2d5f29a
Kim Rutherford <kmr44@cam.ac.uk>
2017-06-03 12:58:51 +1200

    Add a PubMed search link in the literature section
    Refs #378

--- frontend/src/app/gene-references-table/gene-references-table.component.ts
+++ frontend/src/app/gene-references-table/gene-references-table.component.ts
@@ -1,15 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ReferenceShort } from '../pombase-api.service';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
+
+import { ReferenceShort, GeneDetails } from '../pombase-api.service';
 
 @Component({
   selector: 'app-gene-references-table',
   templateUrl: './gene-references-table.component.html',
   styleUrls: ['./gene-references-table.component.css']
 })
-export class GeneReferencesTableComponent implements OnInit {
+export class GeneReferencesTableComponent implements OnInit, OnChanges {
+  @Input() geneDetails: GeneDetails;
   @Input() references: Array<ReferenceShort>;
 
   orderByField = '+authors';
+
+  pubmedSearchUrl = null;
+  pubmedSearchGeneLabel = null;
 
   setOrderBy(field: string) {
     this.orderByField = field;
@@ -18,5 +23,25 @@ export class GeneReferencesTableComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    this.pubmedSearchGeneLabel = this.geneDetails.name || this.geneDetails.uniquename;
+
+    let idList = [this.geneDetails.uniquename];
+
+    if (this.geneDetails.name) {
+      idList.push(this.geneDetails.name);
+    }
+
+    this.geneDetails.synonyms
+      .filter((synonym) => synonym.type !== 'obsolete_name')
+      .map((synonym) => {
+        idList.push(synonym.name);
+      });
+
+    let joinedIds = idList.join(' OR ');
+    this.pubmedSearchUrl =
+      `http://www.ncbi.nlm.nih.gov/sites/entrez?cmd=Search&db=pubmed&term=(${joinedIds}) AND (pombe OR fission yeast)`;
   }
 }
