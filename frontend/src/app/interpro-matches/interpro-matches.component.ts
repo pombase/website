9df7f919af98dfda0de0b81793aa9e62b09622fb
Kim Rutherford <kmr44@cam.ac.uk>
2017-06-04 18:20:06 +1200

    Add header and most links to InterPro table
    Refs #96

--- frontend/src/app/interpro-matches/interpro-matches.component.ts
+++ frontend/src/app/interpro-matches/interpro-matches.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
+
+import { InterProMatch } from '../pombase-api.service';
+import { getGoXrfWithPrefix } from '../config';
 
 @Component({
   selector: 'app-interpro-matches',
   templateUrl: './interpro-matches.component.html',
   styleUrls: ['./interpro-matches.component.css']
 })
-export class InterproMatchesComponent implements OnInit {
+export class InterproMatchesComponent implements OnInit, OnChanges {
   @Input() geneDisplayName;
   @Input() uniprotIdentifier = null;
-  @Input() matches = null;
+  @Input() matches: Array<InterProMatch> = null;
+
+  displayMatches = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges() {
+    this.displayMatches =
+      this.matches.map(match => {
+        let interProEntryUrl = null;
+        if (match.interpro_id) {
+          interProEntryUrl = getGoXrfWithPrefix('InterPro', match.interpro_id);
+        }
+        let dbEntryUrl = getGoXrfWithPrefix(match.dbname, match.id);
+        let newMatch = Object.assign({}, match);
+        newMatch['interProEntryUrl'] = interProEntryUrl;
+        newMatch['dbEntryUrl'] = dbEntryUrl;
+        return newMatch;
+      });
   }
 }
