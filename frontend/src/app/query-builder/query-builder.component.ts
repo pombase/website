362592cbc1ff29d536db5323ccc3c0d088fa079a
Kim Rutherford <kmr44@cam.ac.uk>
2017-06-20 18:24:58 +1200

    Add a loading... spinner to query builder
    Refs pombase/website#99

--- frontend/src/app/query-builder/query-builder.component.ts
+++ frontend/src/app/query-builder/query-builder.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GeneQuery, GeneQueryNode, QueryResult } from '../common/pombase-query';
 import { PombaseAPIService } from '../pombase-api.service';
+import { Subscription } from 'rxjs';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
 
 @Component({
   selector: 'app-query-builder',
@@ -11,6 +13,8 @@ export class QueryBuilderComponent implements OnInit {
   query: GeneQuery;
   results: QueryResult = null;
   history = [];
+  timerSubscription = null;
+  showLoading = false;
 
   resetQuery() {
     this.query = null;
@@ -26,12 +30,20 @@ export class QueryBuilderComponent implements OnInit {
   }
 
   doQuery(saveToHistory: boolean) {
+    this.results = null;
+    this.showLoading = false;
+    let timer = TimerObservable.create(2000, 1000);
+    this.timerSubscription = timer.subscribe(t => {
+      this.showLoading = true;
+    });
     this.pombaseApiService.postQuery(this.query)
       .subscribe((results) => {
         if (saveToHistory) {
           this.history.push(this.query);
         }
         this.results = results;
+        this.timerSubscription.unsubscribe();
+        this.showLoading = false;
       });
   }
 
