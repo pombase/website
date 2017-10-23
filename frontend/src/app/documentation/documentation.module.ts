871d7501f7dbf8990846f6a85cde2814fbc1fba0
Kim Rutherford <kmr44@cam.ac.uk>
2017-10-24 09:22:21 +1300

    Import simplePageScroll so the news links work
    Refs #561

--- frontend/src/app/documentation/documentation.module.ts
+++ frontend/src/app/documentation/documentation.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DocsComponent } from './docs/docs.component';
 
+import { Ng2SimplePageScrollModule } from '../ng2-simple-page-scroll/ng2-simple-page-scroll.module';
+
 import { SharedModule } from '../shared/shared.module';
 
 import { routing } from './documentation.routing';
@@ -10,6 +12,7 @@ import { routing } from './documentation.routing';
   imports: [
     SharedModule,
     CommonModule,
+    Ng2SimplePageScrollModule,
     routing,
   ],
   declarations: [
