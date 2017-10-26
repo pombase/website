a246abbdff8de9859dc0cda5fc599962287718ec
Kim Rutherford <kmr44@cam.ac.uk>
2017-10-26 18:31:03 +1300

    Preload all modules

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { Angulartics2Module, Angulartics2GoogleAnalytics } from 'angulartics2';
 
@@ -128,7 +128,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes),
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
             Angulartics2Module.forRoot([ Angulartics2GoogleAnalytics ])],
   exports: [RouterModule],
   providers: []
