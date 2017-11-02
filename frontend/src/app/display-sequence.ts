pombase/website

commit 9ef0f6ff29b6653643db49c616403064b7d572dd

Colourise the sequence display
Refs #541

frontend/src/app/display-sequence.ts
@@ -1,0 +1,112 @@
+import { FeatureShort } from './pombase-api.service';
+
+export class DisplaySequenceLinePart {
+  constructor(readonly uniquename: string,
+              readonly partType: string,
+              readonly residues: string,
+              readonly exonIndex: number | null) { }
+}
+
+export class DisplaySequenceLine {
+  constructor(readonly lineParts: Array<DisplaySequenceLinePart>) { }
+
+  add(linePart: DisplaySequenceLinePart): void {
+    this.lineParts.push(linePart);
+  }
+
+  length(): number {
+    let len = 0;
+
+    this.lineParts.map(part => {
+      len += part.residues.length;
+    });
+
+    return len;
+  }
+
+  getParts(): Array<DisplaySequenceLinePart> {
+    return this.lineParts;
+  }
+}
+
+export class DisplaySequence {
+  private lines: Array<DisplaySequenceLine> = [new DisplaySequenceLine([])];
+
+  constructor(private readonly lineLength: number,
+              private readonly upstreamSequence: string,
+              private readonly parts: Array<FeatureShort>,
+              private readonly downstreamSequence: string) {
+    const upstreamPart = {
+      feature_type: 'upstream',
+      residues: upstreamSequence,
+      uniquename: 'upstream sequence',
+    } as FeatureShort;
+    const downstreamPart = {
+      feature_type: 'downstream',
+      residues: downstreamSequence,
+      uniquename: 'downstream sequence',
+    } as FeatureShort;
+
+    let allAparts = [upstreamPart, ...parts, downstreamPart];
+
+    let exonIndex = 1;
+    allAparts.map(part => {
+      if (part.feature_type === 'exon') {
+        this.addToLines(part, exonIndex);
+        exonIndex++;
+      } else {
+        this.addToLines(part, null);
+      }
+    });
+  }
+
+  private addToLines(part: FeatureShort, exonIndex: number | null): void {
+    if (part.residues.length === 0) {
+      return;
+    }
+
+    let currentLine = this.lines[this.lines.length - 1];
+
+    let partCopy = {
+      feature_type: part.feature_type,
+      residues: part.residues,
+      uniquename: part.uniquename,
+    } as FeatureShort;
+
+    while (true) {
+      if (currentLine.length() + partCopy.residues.length <= this.lineLength) {
+        const linePart = new DisplaySequenceLinePart(partCopy.uniquename,
+                                                     partCopy.feature_type, partCopy.residues,
+                                                     exonIndex);
+        currentLine.add(linePart);
+
+        if (currentLine.length() === this.lineLength) {
+          this.lines.push(new DisplaySequenceLine([]));
+        }
+
+        return;
+      } else {
+        const sliceLength = this.lineLength - currentLine.length();
+        const sliceResidues = partCopy.residues.slice(0, sliceLength);
+
+        const linePart = new DisplaySequenceLinePart(partCopy.uniquename,
+                                                     partCopy.feature_type, sliceResidues,
+                                                     exonIndex);
+        currentLine.add(linePart);
+
+        const remainingResidues = partCopy.residues.slice(sliceLength);
+
+        partCopy.residues = remainingResidues;
+
+        if (currentLine.length() === this.lineLength) {
+          currentLine = new DisplaySequenceLine([]);
+          this.lines.push(currentLine);
+        }
+      }
+    }
+  }
+
+  getLines(): Array<DisplaySequenceLine> {
+    return this.lines;
+  }
+}

frontend/src/app/transcript-sequence-select/transcript-sequence-select.component.css
@@ -1,4 +1,4 @@
-.controls, .sequence {
+.controls, .sequence-holder {
   display: inline-block;
   vertical-align: top;
 }
@@ -7,12 +7,16 @@
   width: 40%;
 }
 
-.sequence {
-  width: 59%;
+.sequence-holder {
+  width: 50%;
 }
 
-.sequence pre {
+.sequence-display {
   max-height: 800px;
+  font-family: monospace;
+  overflow-y: auto;
+  display: inline-block;
+  background-color: white;
 }
 
 .seq-title {
@@ -20,16 +24,45 @@
   font-weight: bold;
 }
 
-.exon {
-
+.residues {
+  color: #111;
 }
 
-.intron {
-  background-color: #ddf;
+.part-exon, .part-exon-bold {
+  background-color: white;
+  color: black;
 }
 
-.utr {
-  background-color: #ccc;
+.part-exon-bold, .part-exon-bold-even {
+  font-weight: bold;
+}
+
+.part-exon-bold-even, .part-exon-even {
+  background-color: #efe;
+}
+
+.part-exon-bold, .part-exon {
+  background-color: #eef;
+}
+
+.part-five_prime_utr_intron, .part-three_prime_utr_intron, .part-cds_intron {
+  font-style: oblique;
+}
+
+.part-cds_intron {
+  color: #444;
+}
+
+.part-five_prime_utr_intron, .part-three_prime_utr_intron {
+  color: #644;
+}
+
+.part-five_prime_utr, .part-three_prime_utr {
+  color: #0b3dd7;
+}
+
+.part-upstream, .part-downstream {
+  color: #ec7c5f;
 }
 
 .controls .flanking-bases {

frontend/src/app/transcript-sequence-select/transcript-sequence-select.component.html
@@ -99,15 +99,22 @@
           </div>
           </div>
         </div>
-        <div class="sequence">
-          <pre *ngIf="displaySequence">
+        <div class="sequence-holder">
+          <div *ngIf="displaySequence" class="sequence-display">
+          <div class="header">
 &gt;{{sequenceHeader}}
-{{displaySequence}}
-          </pre>
+          </div>
+          <div class="residues">
+          <div *ngFor="let line of displaySequence.getLines()">
+            <span title="{{partTitle(part)}}" class="{{partClass(part)}}"
+                  *ngFor="let part of line.getParts()">{{part.residues}}</span>
+          </div>
           <div *ngIf="sequence != null && wrappedSequence == ''">
-            (No sequence selected)
+            (No sequence to display)
           </div>
           <app-loading-spinner *ngIf="wrappedSequence == null"></app-loading-spinner>
+          </div>
+          </div>
         </div>
     </div>
     <div *ngIf="!hasTranscripts">

frontend/src/app/transcript-sequence-select/transcript-sequence-select.component.ts
@@ -4,6 +4,9 @@ import { saveAs } from 'file-saver';
 
 import { GeneDetails, PombaseAPIService, Strand } from '../pombase-api.service';
 import { Util } from '../shared/util';
+import { DisplaySequence, DisplaySequenceLinePart } from '../display-sequence';
+
+const lineLength = 60;
 
 @Component({
   selector: 'app-transcript-sequence-select',
@@ -15,7 +18,7 @@ export class TranscriptSequenceSelectComponent implements OnChanges {
 
   rawSequence = '';
   wrappedSequence = '';
-  displaySequence = '';
+  displaySequence: DisplaySequence = null;
   sequenceDescription = '';
   sequenceHeader = '';
   hasTranscripts = false;
@@ -71,6 +74,35 @@ export class TranscriptSequenceSelectComponent implements OnChanges {
     }
   }
 
+  partTitle(part: DisplaySequenceLinePart): string {
+    if (part.partType === 'exon') {
+      return part.partType + ' ' + part.exonIndex;
+    } else {
+      return part.partType;
+    }
+  }
+
+  partClass(part: DisplaySequenceLinePart): string {
+    let retVal = 'part-';
+    if (part.partType === 'exon') {
+      if (this.include3PrimeUtr || this.include5PrimeUtr ||
+          this.includeIntrons || this.upstreamBases > 0 ||
+          this.downstreamBases > 0) {
+        retVal += part.partType + '-bold';
+      } else {
+        retVal += part.partType;
+      }
+
+      if (part.exonIndex % 2 === 0 && !this.includeIntrons) {
+        retVal += '-even';
+      }
+    } else {
+      retVal += part.partType;
+    }
+
+    return retVal;
+  }
+
   updateSequence() {
     let transcripts = this.geneDetails.transcripts;
     this.hasTranscripts = transcripts.length > 0;
@@ -87,17 +119,17 @@ export class TranscriptSequenceSelectComponent implements OnChanges {
         this.downstreamBases = 0;
       }
 
+      this.displaySequence = null;
+
       if (this.showTranslation) {
         this.sequenceDescription += '-peptide-sequence';
         this.rawSequence = transcripts[0].protein.sequence;
         this.updateHeader(this.rawSequence.length);
         this.wrappedSequence = Util.splitSequenceString(this.rawSequence);
-        this.displaySequence = this.wrappedSequence;
       } else {
         this.sequenceDescription += '-transcript-sequence';
         this.rawSequence = '';
         this.wrappedSequence = null;
-        this.displaySequence = null;
 
         let geneLocation = this.geneDetails.location;
         let strand;
@@ -158,7 +190,9 @@ export class TranscriptSequenceSelectComponent implements OnChanges {
 
         Promise.all([upstreamPromise, downstreamPromise])
           .then((values) => {
-            let sequence = values[0];
+            const upstreamSequence = values[0];
+            let sequence = upstreamSequence;
+            let includedParts = [];
             for (let part of transcripts[0].parts) {
               if (part.feature_type === 'exon' ||
                   this.includeIntrons && part.feature_type === 'cds_intron' ||
@@ -168,21 +202,24 @@ export class TranscriptSequenceSelectComponent implements OnChanges {
                   this.include3PrimeUtr &&
                   (part.feature_type === 'three_prime_utr' ||
                    this.includeIntrons && part.feature_type === 'three_prime_utr_intron')) {
-                sequence += part.residues;
+                includedParts.push(part);
               }
             }
-            sequence += values[1];
+            const downstreamSequence = values[1];
+            sequence += downstreamSequence;
 
             this.updateHeader(sequence.length);
 
             this.rawSequence = sequence;
             this.wrappedSequence = Util.splitSequenceString(this.rawSequence);
-            this.displaySequence = this.wrappedSequence;
+            this.displaySequence =
+              new DisplaySequence(lineLength,
+                                  upstreamSequence, includedParts, downstreamSequence);
           })
           .catch((e) => {
             this.rawSequence = '';
             this.wrappedSequence = '';
-            this.displaySequence = '';
+            this.displaySequence = null;
           });
       }
     }

commit 82ff32c478750c5af75fe89a1dfb91bdadaea2cf

Fix missing translation, broken in 9ef0f6ff29b66
Refs #541

frontend/src/app/display-sequence.ts
@@ -15,13 +15,17 @@ export class DisplaySequenceLine {
   }
 
   length(): number {
-    let len = 0;
+    return this.residues().length;
+  }
+
+  residues(): string {
+    let res = '';
 
     this.lineParts.map(part => {
-      len += part.residues.length;
+      res += part.residues;
     });
 
-    return len;
+    return res;
   }
 
   getParts(): Array<DisplaySequenceLinePart> {
@@ -108,5 +112,11 @@ export class DisplaySequence {
 
   getLines(): Array<DisplaySequenceLine> {
     return this.lines;
+  }
+
+  residues(): string {
+    let res = '';
+    this.lines.map(line => res += line.residues());
+    return res;
   }
 }

frontend/src/app/transcript-sequence-select/transcript-sequence-select.component.css
@@ -11,12 +11,19 @@
   width: 50%;
 }
 
-.sequence-display {
+.sequence-display, .sequence-holder pre {
   max-height: 800px;
   font-family: monospace;
   overflow-y: auto;
   display: inline-block;
   background-color: white;
+  padding: 0.5em;
+  border-radius: 2px;
+}
+
+.sequence-holder pre {
+  border: 0;
+  font-size: 1em;
 }
 
 .seq-title {

frontend/src/app/transcript-sequence-select/transcript-sequence-select.component.html
@@ -99,8 +99,13 @@
           </div>
           </div>
         </div>
-        <div class="sequence-holder">
-          <div *ngIf="displaySequence" class="sequence-display">
+        <div *ngIf="wrappedSequence" class="sequence-holder">
+          <pre *ngIf="showTranslation">
+&gt;{{sequenceHeader}}
+{{wrappedSequence}}
+          </pre>
+
+          <div class="sequence-display" *ngIf="!showTranslation">
           <div class="header">
 &gt;{{sequenceHeader}}
           </div>

frontend/src/app/transcript-sequence-select/transcript-sequence-select.component.ts
@@ -119,6 +119,7 @@ export class TranscriptSequenceSelectComponent implements OnChanges {
         this.downstreamBases = 0;
       }
 
+      this.wrappedSequence = null;
       this.displaySequence = null;
 
       if (this.showTranslation) {
@@ -129,7 +130,6 @@ export class TranscriptSequenceSelectComponent implements OnChanges {
       } else {
         this.sequenceDescription += '-transcript-sequence';
         this.rawSequence = '';
-        this.wrappedSequence = null;
 
         let geneLocation = this.geneDetails.location;
         let strand;
@@ -191,7 +191,6 @@ export class TranscriptSequenceSelectComponent implements OnChanges {
         Promise.all([upstreamPromise, downstreamPromise])
           .then((values) => {
             const upstreamSequence = values[0];
-            let sequence = upstreamSequence;
             let includedParts = [];
             for (let part of transcripts[0].parts) {
               if (part.feature_type === 'exon' ||
@@ -206,15 +205,16 @@ export class TranscriptSequenceSelectComponent implements OnChanges {
               }
             }
             const downstreamSequence = values[1];
-            sequence += downstreamSequence;
 
-            this.updateHeader(sequence.length);
-
-            this.rawSequence = sequence;
-            this.wrappedSequence = Util.splitSequenceString(this.rawSequence);
             this.displaySequence =
               new DisplaySequence(lineLength,
                                   upstreamSequence, includedParts, downstreamSequence);
+
+            this.rawSequence = this.displaySequence.residues();
+            this.updateHeader(this.rawSequence.length);
+
+            this.wrappedSequence = Util.splitSequenceString(this.rawSequence);
+
           })
           .catch((e) => {
             this.rawSequence = '';