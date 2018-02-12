e1271aaa9a27d660c67f13b970542166d15999ef
Kim Rutherford <kmr44@cam.ac.uk>
2018-02-12 16:31:40 +1300

    Import Observable from a more specific module

--- frontend/src/app/complete.service.ts
+++ frontend/src/app/complete.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import { getAppConfig } from './config';
 
 export class SolrTermSummary {
