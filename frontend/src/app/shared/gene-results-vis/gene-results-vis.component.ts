a1fb7dc33952c4872758d2e70358ca2388f7b327
Kim Rutherford <kmr44@cam.ac.uk>
2018-07-08 20:58:13 +1200

    Refactor to query data needed for vis.
    Refs #842

--- frontend/src/app/shared/gene-results-vis/gene-results-vis.component.ts
+++ frontend/src/app/shared/gene-results-vis/gene-results-vis.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { GeneShort } from '../../pombase-api.service';
 import { getAppConfig, VisColumnConfig } from '../../config';
+import { QueryService } from '../../query.service';
+import { GeneListNode, GeneQuery, QueryOutputOptions, QueryResult } from '../../pombase-query';
+
+class GeneData {
+  constructor(public id: number, height: number, width: number,
+              x: number, y: number, color: string) {};
+}
 
 @Component({
   selector: 'app-gene-results-vis',
@@ -10,14 +17,18 @@ import { getAppConfig, VisColumnConfig } from '../../config';
 export class GeneResultsVisComponent implements OnInit {
   @Input() genes: Array<GeneShort> = [];
 
+  results: QueryResult = null;
+
   geneData = [];
   geneMap = {};
   currentGene = null;
   currentGeneDomId = null;
   visColumnConfigs: Array<VisColumnConfig>;
-  selectedColumns: { [index: string]: boolean; } = {};
 
-  constructor() {
+  selectedColumns: { [index: string]: boolean; } = {};
+  selectedConfigs: Array<VisColumnConfig> = [];
+
+  constructor(private queryService: QueryService) {
     this.visColumnConfigs = getAppConfig().geneResults.visualisation.columns;
 
     this.visColumnConfigs.map(colConfig => {
@@ -49,6 +60,63 @@ export class GeneResultsVisComponent implements OnInit {
     eventTargetElement.setAttribute('fill', geneColor);
   }
 
+  processColumnResults(results: QueryResult): void {
+    this.results = results;
+  }
+
+  queryColumnResults(): void {
+    const geneListNode = new GeneListNode(this.genes);
+    const geneListQuery = new GeneQuery(geneListNode);
+
+    const visColumnNames = this.visColumnConfigs.map(c => c.name);
+    const outputOptions =
+      new QueryOutputOptions(['gene_uniquename', ...visColumnNames], 'none');
+    this.queryService.postQuery(geneListQuery, outputOptions)
+      .subscribe(results => this.processColumnResults(results));
+  }
+
+  makeGeneData(): void {
+    this.geneData = [];
+
+    this.geneMap = {};
+
+    for (let i = 0; i < this.genes.length; i++) {
+      const gene = this.genes[i];
+      const geneDomId = this.makeGeneDomId(gene.uniquename, i);
+
+      this.geneMap[geneDomId] = gene;
+
+      let color = this.geneColor(geneDomId, false);
+
+      this.geneData.push({
+        'id': geneDomId,
+        'height': 3,
+        'width': 500,
+        'x': 0,
+        'y': i * 3,
+        color,
+      });
+    }
+  }
+
+  getGeneData(): Array<GeneData> {
+    return this.geneData;
+  }
+
+  confSelectionChanged(): void {
+    this.selectedConfigs = [];
+    for (const visConfig of this.visColumnConfigs) {
+      if (this.selectedColumns[visConfig.name]) {
+        this.selectedConfigs.push(visConfig);
+      }
+    }
+
+    if (!this.results) {
+      this.queryColumnResults();
+      this.makeGeneData();
+    }
+  }
+
   makeGeneDomId(geneUniquename: string, index: number): string {
     return 'gene-' + index + '-'  + geneUniquename;
   }
@@ -78,27 +146,11 @@ export class GeneResultsVisComponent implements OnInit {
     }
   }
 
+  showResults(): boolean {
+    return this.selectedConfigs.length > 0 && this.results !== null &&
+      this.geneData !== null;
+  }
+
   ngOnInit() {
-    this.geneData = [];
-
-    this.geneMap = {};
-
-    for (let i = 0; i < this.genes.length; i++) {
-      const gene = this.genes[i];
-      const geneDomId = this.makeGeneDomId(gene.uniquename, i);
-
-      this.geneMap[geneDomId] = gene;
-
-      let color = this.geneColor(geneDomId, false);
-
-      this.geneData.push({
-        'id': geneDomId,
-        'height': 2,
-        'width': 500,
-        'x': 0,
-        'y': i * 2,
-        color,
-      });
-    }
   }
 }
