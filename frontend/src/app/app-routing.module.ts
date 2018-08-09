459ead19c0ba1ee897b915d123beaaa008ab59ef
Kim Rutherford <kmr44@cam.ac.uk>
2018-08-09 15:33:20 +1200

    Use the NoPreloading strategy to delay doc loading

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { Routes, RouterModule, NoPreloading } from '@angular/router';
 
 import { Angulartics2Module } from 'angulartics2';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
@@ -163,7 +163,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules,
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: NoPreloading,
                                            scrollPositionRestoration: 'enabled' }),
             Angulartics2Module.forRoot([ Angulartics2GoogleAnalytics ])],
   exports: [RouterModule],
