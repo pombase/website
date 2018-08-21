f3bb267a6eab6c852b292899a9d1c5449eef01fe
Kim Rutherford <kmr44@cam.ac.uk>
2018-08-21 13:26:44 +1200

    Add filter support to the qual and quant sections
    Refs pombase/website#877

--- frontend/src/app/qual-gene-ex-table/qual-gene-ex-table.component.ts
+++ frontend/src/app/qual-gene-ex-table/qual-gene-ex-table.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
 
-import { TermAnnotation } from '../pombase-api.service';
+import { TermAnnotation, AnnotationTable } from '../pombase-api.service';
+import { FilterConfig, AnnotationTableConfig, getAnnotationTableConfig, AnnotationType } from '../config';
+import { AnnotationFilter } from '../filtering/annotation-filter';
+import { TableViewState } from '../pombase-types';
 
 @Component({
   selector: 'app-qual-gene-ex-table',
@@ -10,10 +13,25 @@ import { TermAnnotation } from '../pombase-api.service';
 export class QualGeneExTableComponent implements OnInit, OnChanges {
   @Input() annotationTable: Array<TermAnnotation>;
   @Input() hideColumns: Array<string>;
+  @Input() scope: string;
+  @Output() tableViewChangeEmitter = new EventEmitter<TableViewState>();
 
-  showDetails = false;
   showGene = false;
   showReference = false;
+
+  // copy to the component for use in template
+  TableViewState = TableViewState;
+  currentViewState = TableViewState.Summary;
+
+  config: AnnotationTableConfig = getAnnotationTableConfig();
+  typeConfig: AnnotationType;
+
+  tableIsFiltered = false;
+  filteredAnnotationCount = 0;
+  annotationCount = 0;
+
+  filterConfig: Array<FilterConfig> = null;
+  filteredTable: AnnotationTable = [];
 
   trackByTermId(index: number, item: any) {
     return item.term.termid;
@@ -21,12 +39,41 @@ export class QualGeneExTableComponent implements OnInit, OnChanges {
 
   constructor() { }
 
+  updateCurrentFilter(filter: AnnotationFilter) {
+    if (filter) {
+      [this.filteredTable, this.annotationCount, this.filteredAnnotationCount] =
+        filter.filter(this.annotationTable);
+    } else {
+      this.filteredTable = this.annotationTable;
+    }
+
+    this.tableIsFiltered = !!filter;
+  }
+
+  detailsMode(): boolean {
+    return this.currentViewState == TableViewState.Details;
+  }
+
+  setDetailsView() {
+    this.currentViewState = TableViewState.Details;
+    this.tableViewChangeEmitter.emit(this.currentViewState);
+  }
+
+  setSummaryView() {
+    this.currentViewState = TableViewState.Summary;
+    this.updateCurrentFilter(null);
+    this.tableViewChangeEmitter.emit(this.currentViewState);
+  }
+
   ngOnInit() {
+    this.typeConfig = this.config.getAnnotationType('qualitative_gene_expression');
+    this.filterConfig = this.typeConfig.filters;
   }
 
   ngOnChanges() {
-    this.showDetails = false;
     this.showGene = this.hideColumns.indexOf('gene') === -1;
     this.showReference = this.hideColumns.indexOf('reference') === -1;
+
+    this.setSummaryView();
   }
 }
