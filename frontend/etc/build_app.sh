8d40bbf801613a383ba2e522e6885545505248c6
Kim Rutherford <kmr44@cam.ac.uk>
2018-10-28 20:19:08 +1300

    Display Angular version when building application

--- frontend/etc/build_app.sh
+++ frontend/etc/build_app.sh
@@ -17,6 +17,8 @@ echo Building and releasing using configuration: $release_env
 
 NG='node --max_old_space_size=8192 node_modules/@angular/cli/bin/ng'
 
+$NG --version
+
 if [ x$release_env == x'prod' ]
 then
     echo building $release_env for production ...
