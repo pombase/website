15e7c1d7153c2ee1b85303b31150cf76dba177e9
Kim Rutherford <kmr44@cam.ac.uk>
2019-01-07 21:26:44 +1300

    Update ngx-bootstrap and Angular dependencies

--- frontend/src/app/message-dialog/message-dialog.component.ts
+++ frontend/src/app/message-dialog/message-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { BsModalRef } from 'ngx-bootstrap/modal/';
 
 @Component({
   selector: 'app-message-dialog',
--- frontend/src/app/query-details-dialog/query-details-dialog.component.ts
+++ frontend/src/app/query-details-dialog/query-details-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { BsModalRef } from 'ngx-bootstrap/modal/';
 import { GeneQuery } from '../pombase-query';
 
 @Component({
