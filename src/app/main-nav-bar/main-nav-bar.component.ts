53797f94af4c63dc6f64a415b329bde9a36d3cf8
Kim Rutherford <kmr44@cam.ac.uk>
2019-02-13 17:49:38 +1300

    Add menu items for admin, community curated refs
    Refs pombase/website#1134

--- src/app/main-nav-bar/main-nav-bar.component.ts
+++ src/app/main-nav-bar/main-nav-bar.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { getAppConfig } from '../config';
+
 @Component({
   selector: 'app-main-nav-bar',
   templateUrl: './main-nav-bar.component.html',
   styleUrls: ['./main-nav-bar.component.css']
 })
 export class MainNavBarComponent implements OnInit {
+
+  siteName = getAppConfig().site_name;
 
   constructor() { }
 
