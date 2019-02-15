a784eac13ed688fa2c299c6cf630086435a45044
Kim Rutherford <kmr44@cam.ac.uk>
2019-02-15 19:09:52 +1300

    Fix warning from TSLint

--- src/app/motif-search/motif-search.component.ts
+++ src/app/motif-search/motif-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, OnChanges } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Subject } from 'rxjs/Subject';
