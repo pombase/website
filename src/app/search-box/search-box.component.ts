pombase/website

commit 94b699ea687cbd8d23728b8cb3a265e088bdd201

Add orthologs to download dialog
Refs pombase/website#576

src/app/search-box/search-box.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { PombaseAPIService, GeneSummary, IdAndOrganism } from '../pombase-api.service';
+import { PombaseAPIService, GeneSummary, IdAndOrganism, IdNameAndOrganism } from '../pombase-api.service';
 import { CompleteService, SolrTermSummary, SolrRefSummary } from '../complete.service';
 import { getAppConfig, ConfigOrganism } from '../config';
 
@@ -23,7 +23,7 @@ class SearchSummary {
     public uniprotIdentifierLowerCase: string,
     public synonyms: Array<string>,
     public synonymsLowerCase: Array<string>,
-    public orthologs: Array<IdAndOrganism>,
+    public orthologs: Array<IdNameAndOrganism>,
     public organism: ConfigOrganism) { }
 }
 
@@ -173,7 +173,8 @@ export class SearchBoxComponent implements OnInit {
 
   orthologExactMatch(geneSumm: SearchSummary, value: string): DisplayModel {
     const matchingOrthologs = geneSumm.orthologs.filter(orth => {
-      return orth.identifier.toLowerCase() === value;
+      return orth.identifier.toLowerCase() === value ||
+        orth.name && orth.name.toLowerCase() === value;
     });
     if (matchingOrthologs.length > 0) {
       const details =

src/app/genes-download-dialog/genes-download-dialog.component.html
@@ -11,7 +11,7 @@
         <div class="flex-container">
         <div class="field-column">
         <div>Columns:</div>
-        <div *ngFor="let fieldName of summaryFieldNames">
+        <div *ngFor="let fieldName of fieldNames">
           <input  id="name" type="checkbox" [(ngModel)]="selectedFields[fieldName]"
                   (ngModelChange)="fieldChange(fieldName)"/>
           <label for="name">{{fieldName}}</label>
@@ -77,7 +77,7 @@
         </div>
         <div class="field-column">
            <div>Sequence descriptions:</div>
-           <div *ngFor="let fieldName of summaryFieldNames">
+           <div *ngFor="let fieldName of fieldNames">
              <input id="name" type="checkbox" [(ngModel)]="selectedFields[fieldName]"
                     [disabled]="fieldName === 'Systematic ID'"
                     (ngModelChange)="fieldChange(fieldName)"/>

src/app/genes-download-dialog/genes-download-dialog.component.ts
@@ -5,13 +5,12 @@ import { BsModalRef } from 'ngx-bootstrap/modal/';
 import { TabsetComponent } from 'ngx-bootstrap';
 
 import { GeneQuery, GeneListNode, QueryOutputOptions, FormatUtils,
-         FormatTypes,
-         ResultRow} from '../pombase-query';
+         FormatTypes, ResultRow} from '../pombase-query';
 import { QueryService } from '../query.service';
 import { getAppConfig, AppConfig } from '../config';
 import { DeployConfigService } from '../deploy-config.service';
 
-import { GeneShort, GeneSummary, PombaseAPIService } from '../pombase-api.service';
+import { GeneShort, GeneSummary, PombaseAPIService, GeneSummaryMap } from '../pombase-api.service';
 
 @Component({
   selector: 'app-genes-download-dialog',
@@ -32,11 +31,12 @@ export class GenesDownloadDialogComponent implements OnInit {
   public upstreamBases = 0;
   public downstreamBases = 0;
 
-  summaryFieldNames = ['Systematic ID', 'Name', 'Product description', 'UniProt ID',
+  fieldNames = ['Systematic ID', 'Name', 'Product description', 'UniProt ID',
                 'Synonyms', 'Feature type', 'Start position', 'End position',
                 'Chromosome', 'Strand'];
+
   selectedFields: { [key: string]: boolean } = {'Systematic ID': true};
-  summaryFieldValGenerators: { [label: string]: (g: GeneSummary) => string } = {
+  fieldValGenerators: { [label: string]: (g: GeneSummary) => string } = {
     'Systematic ID': g => g.uniquename,
     'Name': g => g.name || '',
     'Synonyms': g => g.synonyms.join(','),
@@ -56,6 +56,8 @@ export class GenesDownloadDialogComponent implements OnInit {
     'Strand': g => g.location.strand,
   };
 
+  summaryPromise: Promise<GeneSummaryMap> = null;
+
   fieldsForServer =
     getAppConfig().getGeneResultsConfig().visualisation.columns
     .filter(conf => conf.column_type !== 'ortholog');
@@ -63,7 +65,20 @@ export class GenesDownloadDialogComponent implements OnInit {
   constructor(private pombaseApiService: PombaseAPIService,
               private queryService: QueryService,
               public bsModalRef: BsModalRef,
-              public deployConfigService: DeployConfigService) {}
+              public deployConfigService: DeployConfigService) {
+
+    this.summaryPromise = this.pombaseApiService.getGeneSummaryMapPromise();
+
+    for (const orthTaxonid of this.appConfig.ortholog_taxonids) {
+      const orthOrg = this.appConfig.getOrganismByTaxonid(orthTaxonid);
+      const orthFieldName = orthOrg.common_name + ' ortholog(s)';
+      this.fieldNames.push(orthFieldName);
+      this.fieldValGenerators[orthFieldName] =
+        (summary) =>
+          summary.orthologs.filter(orth => orth.taxonid === orthTaxonid)
+            .map(orth => orth.name || orth.identifier).join(',');
+    }
+  }
 
   private currentTab(): string {
     if (this.staticTabs.tabs[0].active) {
@@ -74,7 +89,7 @@ export class GenesDownloadDialogComponent implements OnInit {
   }
 
   selectAll() {
-    this.summaryFieldNames.map(name => this.selectedFields[name] = true);
+    this.fieldNames.map(name => this.selectedFields[name] = true);
   }
 
   fieldChange(fieldName: string) {
@@ -108,7 +123,7 @@ export class GenesDownloadDialogComponent implements OnInit {
   }
 
   isValid() {
-    for (let fieldName of this.summaryFieldNames) {
+    for (let fieldName of this.fieldNames) {
       if (this.selectedFields[fieldName]) {
         return true;
       }
@@ -117,15 +132,13 @@ export class GenesDownloadDialogComponent implements OnInit {
   }
 
   private selectedFieldNames(): [Array<string>, Array<string>] {
-    return [this.summaryFieldNames.filter(name => this.selectedFields[name]),
+    return [this.fieldNames.filter(name => this.selectedFields[name]),
             this.fieldsForServer.map(conf => conf.name)
                .filter(fieldName => this.selectedFields[fieldName])];
   }
 
   private downloadDelimited() {
     const [selectedSummaryFields, selectedServerFields] = this.selectedFieldNames();
-
-    const summaryPromise = this.pombaseApiService.getGeneSummaryMapPromise();
 
     let serverPromise;
 
@@ -138,7 +151,7 @@ export class GenesDownloadDialogComponent implements OnInit {
       serverPromise = Promise.resolve(null);
     }
 
-    Promise.all([summaryPromise, serverPromise])
+    Promise.all([this.summaryPromise, serverPromise])
       .then(([geneSummaries, serverResults]) => {
         const serverRowsMap: { [index: string]: ResultRow } = {};
 
@@ -165,7 +178,7 @@ export class GenesDownloadDialogComponent implements OnInit {
 
           let row = [];
           for (const fieldName of selectedSummaryFields) {
-            let fieldVal = this.summaryFieldValGenerators[fieldName](geneSummary);
+            let fieldVal = this.fieldValGenerators[fieldName](geneSummary);
 
             row.push(fieldVal);
           }
@@ -226,7 +239,7 @@ export class GenesDownloadDialogComponent implements OnInit {
 
           const headerFields = geneUniquename + ' ' +
             selectedSummaryFields.filter(fieldName => fieldName !== 'Systematic ID')
-              .map(fieldName => this.summaryFieldValGenerators[fieldName](geneSummary))
+              .map(fieldName => this.fieldValGenerators[fieldName](geneSummary))
               .join('|');
 
           descriptions[geneUniquename] = headerFields;

src/app/pombase-api.service.ts
@@ -207,6 +207,12 @@ export interface IdAndOrganism {
   taxonid: number;
 }
 
+export interface IdNameAndOrganism {
+  identifier: string;
+  name: string | null;
+  taxonid: number;
+}
+
 export interface GeneSummary extends GeneShort {
   uniquename: string;
   name: string;
@@ -215,7 +221,7 @@ export interface GeneSummary extends GeneShort {
   product?: string;
   uniprot_identifier?: string;
   synonyms: Array<string>;
-  orthologs: Array<IdAndOrganism>;
+  orthologs: Array<IdNameAndOrganism>;
   location?: ChromosomeLocation;
   feature_type: string;
 }

commit 8ac7d0044b49b622f1ca546f2ad63f9327ed5e34

Show orth. match name not identifier in search box
Broken by 94b699ea687cbd8d23728b8cb

Refs pombase/website#576

src/app/search-box/search-box.component.ts
@@ -74,7 +74,7 @@ export class SearchBoxComponent implements OnInit {
     return new DisplayModel('Matching genes:', uniquename, name, otherDetails, organism);
   }
 
-  highlightMatch(pos: number, searchString: string, target?: string): string {
+    highlightMatch(pos: number, searchString: string, target?: string): string {
     let start;
     let highlightBit;
     let rest;
@@ -155,33 +155,44 @@ export class SearchBoxComponent implements OnInit {
     return null;
   }
 
-  orthologMatch(geneSumm: SearchSummary, value: string): DisplayModel {
-    for (let orth of geneSumm.orthologs) {
-      const pos = orth.identifier.toLowerCase().indexOf(value);
-      if (pos !== -1) {
-        const orgDetails = getAppConfig().getOrganismByTaxonid(orth.taxonid);
-        const commonNameSpan =
-          `<span class="search-box-organism">(${orgDetails.common_name})</span>`;
-        const detail =
-          `ortholog: ${this.highlightMatch(pos, value, orth.identifier)} ${commonNameSpan}`;
-        return this.makeGeneDisplayModel(geneSumm.uniquename, geneSumm.name,
-                                         [detail], geneSumm.organism);
+  orthologMatch(geneSumm: SearchSummary, value: string, exactness: 'exact'|'sub-string'): DisplayModel {
+    interface FieldAndOrth {
+      matchingFieldValue: string;
+      orth: IdNameAndOrganism;
+    }
+
+    let matchingOrthologs: Array<FieldAndOrth> = [];
+
+    if (exactness === 'exact') {
+      for (const orth of geneSumm.orthologs) {
+        if (orth.name && orth.name.toLowerCase().indexOf(value) !== -1) {
+          matchingOrthologs.push({ matchingFieldValue: orth.name, orth });
+          break;
+        } else {
+          if (orth.identifier.toLowerCase().indexOf(value) !== -1) {
+            matchingOrthologs.push({ matchingFieldValue: orth.identifier, orth });
+            break;
+          }
+        }
+      }
+    } else {
+      for (const orth of geneSumm.orthologs) {
+        if (orth.name && orth.name.toLowerCase() === value) {
+          matchingOrthologs.push({ matchingFieldValue: orth.name, orth });
+        } else {
+          if (orth.identifier.toLowerCase() === value) {
+            matchingOrthologs.push({ matchingFieldValue: orth.identifier, orth });
+          }
+        }
       }
     }
-    return null;
-  }
 
-  orthologExactMatch(geneSumm: SearchSummary, value: string): DisplayModel {
-    const matchingOrthologs = geneSumm.orthologs.filter(orth => {
-      return orth.identifier.toLowerCase() === value ||
-        orth.name && orth.name.toLowerCase() === value;
-    });
     if (matchingOrthologs.length > 0) {
       const details =
-        matchingOrthologs.map(orth => {
+        matchingOrthologs.map(({ matchingFieldValue, orth })  => {
           const orgDetails = getAppConfig().getOrganismByTaxonid(orth.taxonid);
           const commonNameSpan = `<span class="search-box-organism">(${orgDetails.common_name})</span>`;
-          return `ortholog: ${this.highlightMatch(0, orth.identifier)} ${commonNameSpan}`;
+          return `ortholog: ${this.highlightMatch(0, matchingFieldValue)} ${commonNameSpan}`;
         });
       return this.makeGeneDisplayModel(geneSumm.uniquename, geneSumm.name,
         details, geneSumm.organism);
@@ -245,7 +256,7 @@ export class SearchBoxComponent implements OnInit {
           maybeAdd(this.synonymExactMatch(geneSumm, value));
         }
         for (let geneSumm of this.searchSummaries) {
-          maybeAdd(this.orthologExactMatch(geneSumm, value));
+          maybeAdd(this.orthologMatch(geneSumm, value, 'exact'));
         }
         for (let geneSumm of this.searchSummaries) {
           maybeAdd(this.nameMatch(geneSumm, value));
@@ -265,7 +276,7 @@ export class SearchBoxComponent implements OnInit {
         }
         if (filteredSummaries.length < 20) {
           for (let geneSumm of this.searchSummaries) {
-            maybeAdd(this.orthologMatch(geneSumm, value));
+            maybeAdd(this.orthologMatch(geneSumm, value, 'sub-string'));
           }
         }
         if (filteredSummaries.length < 20) {