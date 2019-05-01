pombase/website

commit 1e9a951ad886c9418f84c30bb57ed6da8d12037d

Code tidy: don't overload GeneQuery constructor

src/app/pombase-query.ts
@@ -483,7 +483,18 @@ export class GeneQuery {
   private name: string;
   private stringQuery: string = null;
 
-  private makeNode(parsedJson: any): GeneQueryNode {
+  static fromJSONString(arg: { name: string, constraints: string }): GeneQuery {
+    if (typeof(arg) === 'string') {
+      throw new Error('GeneQuery constructor needs an Object not a string');
+    }
+    if (arg['constraints']) {
+      return new GeneQuery(arg['name'], this.nodeFromObj(arg['constraints']))
+    } else {
+      return new GeneQuery(null, this.nodeFromObj(arg));
+    }
+  }
+
+  private static nodeFromObj(parsedJson: any): GeneQueryNode {
     const keys = Object.keys(parsedJson);
     if (keys.length !== 1) {
       throw new Error('parsedJson doesn\'t have exactly one key' + parsedJson);
@@ -501,7 +512,7 @@ export class GeneQuery {
     case 'or':
     case 'and':
     case 'not':
-      const parts = (val as Array<GeneQueryNode>).map((json: any) => this.makeNode(json));
+      const parts = (val as Array<GeneQueryNode>).map((json: any) => this.nodeFromObj(json));
       return new GeneBoolNode(nodeType, parts);
 
     case 'subset':
@@ -526,22 +537,10 @@ export class GeneQuery {
     throw new Error('Unknown type: ' + nodeType);
   }
 
-  constructor(arg: string | GeneQueryNode | { name: string, constraints: string }) {
+  constructor(queryName: string, topNode: GeneQueryNode) {
     this.name = null;
     this.queryId = nextQueryId++;
-    if (arg instanceof GeneQueryNode) {
-      this.queryTopNode = arg;
-    } else {
-      if (typeof(arg) === 'string') {
-        throw new Error('GeneQuery constructor needs an Object not a string');
-      }
-      if (arg['constraints']) {
-        this.queryTopNode = this.makeNode(arg['constraints']);
-        this.name = arg['name'];
-      } else {
-        this.queryTopNode = this.makeNode(arg);
-      }
-    }
+    this.queryTopNode = topNode;
 
     this.stringQuery = this.getTopNode().toString();
   }

src/app/genes-download-dialog/genes-download-dialog.component.ts
@@ -111,7 +111,7 @@ export class GenesDownloadDialogComponent implements OnInit {
     let serverPromise;
 
     if (selectedServerFields.length > 0) {
-      const query = new GeneQuery(new GeneListNode(this.genes));
+      const query = new GeneQuery(null, new GeneListNode(this.genes));
       const outputOptions = new QueryOutputOptions(['gene_uniquename', ...selectedServerFields], [], 'none');
       serverPromise = this.queryService.postQuery(query, outputOptions);
 
@@ -191,7 +191,7 @@ export class GenesDownloadDialogComponent implements OnInit {
     const [selectedSummaryFields] = this.selectedFieldNames();
     const summaryMapPromise = this.pombaseApiService.getGeneSummaryMapPromise();
 
-    const query = new GeneQuery(new GeneListNode(this.genes));
+    const query = new GeneQuery(null, new GeneListNode(this.genes));
     let seqOptions = this.seqDownloadOptions();
     const outputOptions = new QueryOutputOptions(['gene_uniquename'], [], seqOptions);
     const queryPromise = this.queryService.postQuery(query, outputOptions);

src/app/motif-search/motif-search.component.ts
@@ -103,7 +103,7 @@ export class MotifSearchComponent implements OnInit {
   sendToQueryBuilder(): void {
     const geneUniquenames = this.peptideResults.map(res => res.gene_id);
     const part = new GeneListNode(geneUniquenames);
-    const geneQuery = new GeneQuery(part);
+    const geneQuery = new GeneQuery(null, part);
     const callback = (historyEntry: HistoryEntry) => {
       this.router.navigate(['/query/results/from/history/', historyEntry.getEntryId()]);
     };

src/app/query-builder/query-builder.component.ts
@@ -45,7 +45,7 @@ export class QueryBuilderComponent implements OnInit, OnDestroy {
         params['saveOrResults'] && params['saveOrResults'] === 'results';
       if (params['predefinedQueryId']) {
         const queryJson = getAppConfig().getPredefinedQuery(params['predefinedQueryId']);
-        const query = new GeneQuery(queryJson);
+        const query = GeneQuery.fromJSONString(queryJson);
         if (goToResults) {
           this.gotoResults(query);
         } else {
@@ -98,7 +98,7 @@ export class QueryBuilderComponent implements OnInit, OnDestroy {
   private fromSubsetName(goToResults: boolean,
                          subsetName: string, subsetDisplayName: string): void {
     const constraints = new SubsetNode (subsetName, subsetDisplayName);
-    const query = new GeneQuery(constraints);
+    const query = new GeneQuery(null, constraints);
     if (goToResults) {
       this.gotoResults(query);
     } else {
@@ -108,7 +108,7 @@ export class QueryBuilderComponent implements OnInit, OnDestroy {
 
   private fromJson(goToResults: boolean, json: string) {
     const obj = JSON.parse(json);
-    const query = new GeneQuery(obj);
+    const query = GeneQuery.fromJSONString(obj);
     if (goToResults) {
       this.gotoResults(query);
     } else {
@@ -128,7 +128,7 @@ export class QueryBuilderComponent implements OnInit, OnDestroy {
       }
       const termName = decodeURIComponent(encodedTermName);
       const constraints = new TermNode(termId, termName, null, singleOrMulti, null);
-      newQuery = new GeneQuery(constraints);
+      newQuery = new GeneQuery(null, constraints);
     }
 
     if (newQuery) {
@@ -166,7 +166,7 @@ export class QueryBuilderComponent implements OnInit, OnDestroy {
 
   nodeEvent({ node, nodeConf }: {node: GeneQueryNode, nodeConf: QueryNodeConfig}) {
     if (node) {
-      const query = new GeneQuery(node);
+      const query = new GeneQuery(null, node);
       this.saveQuery(query);
 //   See #1236 - maybe add the UniProt ID column to the results table if the
 //   user searches using the UniProt IDs tools

src/app/query-history/query-history.component.ts
@@ -31,7 +31,7 @@ export class QueryHistoryComponent implements OnInit, OnDestroy {
       this.getSelectedEntries().map(e => e.getQuery().getTopNode());
     let node = new GeneBoolNode(op, selectedQueryNodes);
     this.historyEntries.map((histEntry) => histEntry.checked = false);
-    this.queryService.saveToHistory(new GeneQuery(node));
+    this.queryService.saveToHistory(new GeneQuery(null, node));
   }
 
   getEntryDisplayString(histEntry: HistoryEntry): string {

src/app/query-node/query-node.component.ts
@@ -41,7 +41,7 @@ export class QueryNodeComponent implements OnInit, OnChanges {
     this.cannedQueryDetails =
       getAppConfig().cannedQueryIds.map(id => {
         const queryId = 'canned_query:' + id;
-        const query = new GeneQuery(getAppConfig().getPredefinedQuery(queryId));
+        const query = GeneQuery.fromJSONString(getAppConfig().getPredefinedQuery(queryId));
         return {
           name: query.getName(),
           queryId: queryId,
@@ -176,7 +176,7 @@ export class QueryNodeComponent implements OnInit, OnChanges {
 
   selectPredefinedQuery(predefinedQueryId: string): void {
     const queryJson = getAppConfig().getPredefinedQuery(predefinedQueryId);
-    const query = new GeneQuery(queryJson);
+    const query = GeneQuery.fromJSONString(queryJson);
     this.emitNodeEvent(query.getTopNode());
   }
 }

src/app/query.service.ts
@@ -76,7 +76,7 @@ export class QueryService {
 
         for (let o of JSON.parse(savedHistoryString)) {
           try {
-            const query = new GeneQuery(o.constraints);
+            const query = GeneQuery.fromJSONString(o.constraints);
             const entry = new HistoryEntry(query, o.resultCount);
             this.history.push(entry);
           } catch (e) {
@@ -112,12 +112,12 @@ export class QueryService {
   }
 
   postPredefinedQuery(queryName: string, outputOptions: QueryOutputOptions): Promise<QueryResult> {
-    const query = new GeneQuery(getAppConfig().getPredefinedQuery(queryName));
+    const query = GeneQuery.fromJSONString(getAppConfig().getPredefinedQuery(queryName));
     return this.postQuery(query, outputOptions);
   }
 
   postPredefinedQueryCount(queryName: string): Promise<number> {
-    const query = new GeneQuery(getAppConfig().getPredefinedQuery(queryName));
+    const query = GeneQuery.fromJSONString(getAppConfig().getPredefinedQuery(queryName));
     return this.postQueryCount(query);
   }
 

src/app/shared/gene-results-slim-table/gene-results-slim-table.component.ts
@@ -34,7 +34,7 @@ export class GeneResultsSlimTableComponent implements OnInit {
 
   runQuery(slimName: string): void {
     const geneListNode = new GeneListNode(this.genes);
-    const geneListQuery = new GeneQuery(geneListNode);
+    const geneListQuery = new GeneQuery(null, geneListNode);
 
     const outputOptions =
       new QueryOutputOptions(['gene_uniquename'], ['include_gene_subsets'], 'none');
@@ -123,7 +123,7 @@ export class GeneResultsSlimTableComponent implements OnInit {
 
   private gotoGenes(genes: string[]) {
     const part = new GeneListNode(genes);
-    const geneQuery = new GeneQuery(part);
+    const geneQuery = new GeneQuery(null, part);
     const callback = (historyEntry: HistoryEntry) => {
       this.router.navigate(['/query/results/from/history/', historyEntry.getEntryId()]);
     };

commit d72a4d5e92e2414e466b39a74a5e48232f00577b

Fix missing predefined queries broken by 1e9a951ad
1e9a951ad886c9418f84c30bb57ed6da8d12037d

src/app/pombase-query.ts
@@ -538,7 +538,7 @@ export class GeneQuery {
   }
 
   constructor(queryName: string, topNode: GeneQueryNode) {
-    this.name = null;
+    this.name = queryName;
     this.queryId = nextQueryId++;
     this.queryTopNode = topNode;
 