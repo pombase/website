277abf833c7052e3e2d013c9428371694ae0c6c8
Kim Rutherford <kmr44@cam.ac.uk>
2019-06-09 01:41:29 +1200

    Include string-similarity code directly
    The module from NPM has a module.exports that doesn't work on IE:
    https://github.com/aceakash/string-similarity/issues/38

--- src/app/complete.service.ts
+++ src/app/complete.service.ts
@@ -6,7 +6,41 @@ import 'rxjs/add/observable/from';
 
 import { getAppConfig } from './config';
 
-import * as similarity from 'string-similarity';
+function compareTwoStrings(first: string, second: string) {
+  first = first.replace(/\s+/g, '')
+  second = second.replace(/\s+/g, '')
+
+  if (!first.length && !second.length) { return 1; }                   // if both are empty strings
+  if (!first.length || !second.length) { return 0; }                   // if only one is empty string
+  if (first === second) { return 1; }       							 // identical
+  if (first.length === 1 && second.length === 1) { return 0; }         // both are 1-letter strings
+  if (first.length < 2 || second.length < 2) { return 0; }			 // if either is a 1-letter string
+
+  let firstBigrams = new Map();
+  for (let i = 0; i < first.length - 1; i++) {
+    const bigram = first.substr(i, 2);
+    const count = firstBigrams.has(bigram)
+      ? firstBigrams.get(bigram) + 1
+      : 1;
+
+    firstBigrams.set(bigram, count);
+  };
+
+  let intersectionSize = 0;
+  for (let i = 0; i < second.length - 1; i++) {
+    const bigram = second.substr(i, 2);
+    const count = firstBigrams.has(bigram)
+      ? firstBigrams.get(bigram)
+      : 0;
+
+    if (count > 0) {
+      firstBigrams.set(bigram, count - 1);
+      intersectionSize++;
+    }
+  }
+
+  return (2.0 * intersectionSize) / (first.length + second.length - 2);
+}
 
 export class SolrTermSummary {
   constructor(public termid: string, public name: string, public definition: string) {};
@@ -46,9 +80,9 @@ export class CompleteService {
         const resultTerms = terms.map((term: any) => {
           let synonymMatch = null;
           if (queryText.length >= 2 && term['close_synonyms']) {
-            const nameScore = similarity.compareTwoStrings(term.name, queryText);
+            const nameScore = compareTwoStrings(term.name, queryText);
             for (const syn of term['close_synonyms'] as Array<string>) {
-              const synScore = similarity.compareTwoStrings(syn, queryText);
+              const synScore = compareTwoStrings(syn, queryText);
               if (synScore > nameScore) {
                 synonymMatch = syn;
                 break;
