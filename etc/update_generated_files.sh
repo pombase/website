4efb8458b44227f08366f792fffeaef7aafb83df
Kim Rutherford <kmr@aska.gen.nz>
2019-11-27 17:44:45 +1300

    Display Pandoc version before generating HTML

--- etc/update_generated_files.sh
+++ etc/update_generated_files.sh
@@ -12,4 +12,7 @@ ABBREVS="AGI_LocusCode CDD DDJB EC ECOGENE EMBL ENSEMBL FB GEO GO Gene3D HAMAP I
 
 etc/make-link-js.pl $go_xrfs_abbs $ABBREVS > src/app/config/go-xrf-abbr-external-links.json
 
+echo -n 'Using Pandoc version: '
+pandoc --version
+
 ./etc/process-markdown.pl --web-config $web_config --doc-config src/app/config/doc-config.json --markdown-docs src/docs --recent-news-component src/app/recent-news/recent-news.component.html --docs-component src/app/documentation/docs/docs.component.html --front-panel-content-component src/app/front-panel-content/front-panel-content.component.html
