dc486d0cc82e9f615bfa8db5457be3afc445348a
Kim Rutherford <kmr@aska.gen.nz>
2019-12-31 02:52:07 +1300

    Add DOI to xrf file
    Refs pombase/website#1433

--- etc/update_generated_files.sh
+++ etc/update_generated_files.sh
@@ -8,7 +8,7 @@ set -o pipefail
 go_xrfs_abbs=$1
 web_config=$2
 
-ABBREVS="AGI_LocusCode CDD DDJB EC ECOGENE EMBL ENSEMBL FB GEO GO Gene3D HAMAP InterPro iPTMnet JCVI_TIGRFAMS MGI ModBase PANTHER PDB PIRSF PR PRINTS PRODOM Pfam ProDom Prosite QuickGO RGD SFLD SGD SMART SO SUPERFAMILY UniPathway UniProtKB UniProtKB-KW UniProtKB-SubCell WB dictyBase Cyclebase"
+ABBREVS="AGI_LocusCode CDD DDJB DOI EC ECOGENE EMBL ENSEMBL FB GEO GO Gene3D HAMAP InterPro iPTMnet JCVI_TIGRFAMS MGI ModBase PANTHER PDB PIRSF PR PRINTS PRODOM Pfam ProDom Prosite QuickGO RGD SFLD SGD SMART SO SUPERFAMILY UniPathway UniProtKB UniProtKB-KW UniProtKB-SubCell WB dictyBase Cyclebase"
 
 etc/make-link-js.pl $go_xrfs_abbs $ABBREVS > src/app/config/go-xrf-abbr-external-links.json
 
