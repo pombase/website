a076fc79f859c0e9cfddc69906c7eca6150b36a5
Kim Rutherford <kmr@aska.gen.nz>
2020-02-21 16:45:51 +1300

    Import animation module after other Angular mods
    I hoped this would fix a build bug but doesn't.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { TypeaheadModule, PaginationModule, PopoverModule } from 'ngx-bootstrap';
