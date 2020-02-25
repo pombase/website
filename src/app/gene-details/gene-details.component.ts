b957bf9b30addd8cc76958df27044d1a9e50c7ea
Kim Rutherford <kmr@aska.gen.nz>
2020-02-25 20:53:44 +1300

    Show the full organism name on non-pombe pages

--- src/app/gene-details/gene-details.component.ts
+++ src/app/gene-details/gene-details.component.ts
@@ -29,6 +29,7 @@ export class GeneDetailsComponent implements OnInit {
   showProteinFeatures = false;
   productSize = '';
   organism: ConfigOrganism = null;
+  organismLongName: string = null;
   jbrowseLinkUrl: string = null;
   sanitizedJBrowseURL: SafeResourceUrl = null;
   extraMenuSections = [
@@ -319,6 +320,7 @@ export class GeneDetailsComponent implements OnInit {
                 !!this.geneDetails.cv_annotations['PomBase family or domain'];
               this.setVisibleSections();
               this.organism = this.appConfig.getOrganismByTaxonid(geneDetails.taxonid);
+              this.organismLongName = this.organism.genus + ' ' + this.organism.species;
               this.apiError = null;
             })
             .catch((error: any) => {
