dbd41b27b0760e2c4d51b5dde7e55acbc75cd573
Kim Rutherford <kmr@aska.gen.nz>
2020-05-17 00:08:50 +1200

    Go to 404 page for unknown PMIDs
    Instead of showing the raw error

--- src/app/pombase-api.service.ts
+++ src/app/pombase-api.service.ts
@@ -544,7 +544,7 @@ export class PombaseAPIService {
 
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error);
-    return Promise.reject(error.message || error);
+    return Promise.reject(error);
   }
 
   processAlleleMap(allelesByUniquename: AlleleMap, genesByUniquename: GeneMap) {
