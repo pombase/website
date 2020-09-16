pombase/website

commit 2e0d339b100775ea02e87c5063121779eb4d9bb3

Use timer() not TimerObservable

src/app/query.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Subject, BehaviorSubject } from 'rxjs';
-import { TimerObservable } from 'rxjs/observable/TimerObservable';
+import { Subject, BehaviorSubject, timer } from 'rxjs';
 import { GeneQuery, QueryResult, QueryIdNode, GeneUniquename, ResultRow, TermAndName } from './pombase-query';
 import { getAppConfig } from './config';
 import { PombaseAPIService, GeneSummaryMap, GeneSummary } from './pombase-api.service';
@@ -126,10 +125,12 @@ export class QueryService {
           }
         };
 
-        let timer = TimerObservable.create(0.2);
-        timer.subscribe(() => {
+        const timer$ = timer(0.2);
+        const subscription = timer$.subscribe(() => {
           this.setAllCounts();
         });
+
+        subscription.unsubscribe();
       }
       this.subject = new BehaviorSubject(this.history);
     } catch (e) {
@@ -420,8 +421,8 @@ export class QueryService {
       (histEntry: HistoryEntry, delay: number) => {
         const query = histEntry.getQuery();
 
-        let timer = TimerObservable.create(delay);
-        const subscription = timer.subscribe(() => {
+        let timer$ = timer(delay);
+        const subscription = timer$.subscribe(() => {
           this.postQueryCount(query)
             .then((res) => {
               histEntry.setUpdatedCount(res.getRowCount());

commit 7d0931ea62f90fb6ba3c76d0f4849e571acbbf5a

Fix history result count updating
Broken by 2e0d339b100775ea02e87c5063121779eb4d9bb3

Refs pombase/website#1604

src/app/query.service.ts
@@ -125,12 +125,12 @@ export class QueryService {
           }
         };
 
-        const timer$ = timer(0.2);
+        const timer$ = timer(200);
         const subscription = timer$.subscribe(() => {
           this.setAllCounts();
+          subscription.unsubscribe();
         });
 
-        subscription.unsubscribe();
       }
       this.subject = new BehaviorSubject(this.history);
     } catch (e) {