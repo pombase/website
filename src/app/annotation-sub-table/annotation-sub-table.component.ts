pombase/website

commit 40ae3c02fa96ef1291946ba39f7c3b9ed8030d1a

Fix more Typescript warnings

src/app/annotation-sub-table/annotation-sub-table.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit, Input, Output, OnChanges, EventEmitter } from '@angular/core';
-import { TermAnnotation, Annotation, ReferenceShort } from '../pombase-api.service';
+import { TermAnnotation, ReferenceShort } from '../pombase-api.service';
 
 import { getAnnotationTableConfig, AnnotationTableConfig, AnnotationType,
          FilterConfig, SplitByParentsConfig, AnnotationExternalLinkConfig,
          getAppConfig,
-         AppConfig,
-         LinkDisplay} from '../config';
+         AppConfig} from '../config';
 import { AnnotationTable } from '../pombase-api.service';
 import { TableViewState } from '../pombase-types';
-import { TermShort, TermXref } from '../pombase-query';
+import { TermShort } from '../pombase-query';
 import { Filter } from '../filtering';
 
 @Component({
@@ -122,7 +121,7 @@ export class AnnotationSubTableComponent implements OnInit, OnChanges {
     this.tableViewChangeEmitter.emit(this.currentViewState);
   }
 
-  trackByTermId(index: number, item: any) {
+  trackByTermId(item: any) {
     return item.term.termid;
   }
 

src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Title } from '@angular/platform-browser';
 
 import { filter, map, mergeMap } from 'rxjs/operators';
 
-import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
-
 import { getAppConfig } from './config';
 
 @Component({

src/app/display-sequence.ts
@@ -74,7 +74,7 @@ export class DisplaySequence {
   }
 
   private constructor(private readonly lineLength: number,
-                      private readonly displayParts: Array<RawSequencePart>) {
+                      public readonly displayParts: Array<RawSequencePart>) {
     let exonIndex = 1;
     displayParts.map(part => {
       if (part.partType === 'exon') {

src/app/filtering.ts
@@ -20,7 +20,7 @@ export class FilterCombiner<T> implements Filter<T> {
       filterResult = filter.filter(filterResult[0]);
     }
 
-    let [filteredTable, _, filteredAnnotationCount] = filterResult;
+    let [filteredTable, , filteredAnnotationCount] = filterResult;
 
     return [filteredTable, totalAnnotationCount, filteredAnnotationCount];
   }

src/app/filtering/annotation-extension-filter.ts
@@ -1,4 +1,4 @@
-import { AnnotationTable, Annotation } from '../pombase-api.service';
+import { AnnotationTable } from '../pombase-api.service';
 import { TermShort } from '../pombase-query';
 import { Filter } from '../filtering';
 

src/app/gene-external-references/gene-external-references.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnChanges, Input } from '@angular/core';
 
-import { getAppConfig, ExternalGeneReference } from '../config';
-
 import { GeneDetails } from '../pombase-api.service';
 
 @Component({

src/app/interaction-table-filters/interaction-table-filters.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, Input, Output, OnChanges,
-         EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { InteractionAnnotation, InteractionTable } from '../pombase-api.service';
 import { FilterConfig } from '../config';
 import { InteractionFilter, FilterCombiner } from '../filtering';
-import { TableViewState } from '../pombase-types';
 
 @Component({
   selector: 'app-interaction-table-filters',

src/app/interaction-table-throughput-filter/interaction-table-throughput-filter.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
-import { InteractionTable, InteractionAnnotation, ThroughputType } from '../pombase-api.service';
+import { InteractionTable, ThroughputType } from '../pombase-api.service';
 import { FilterConfig } from '../config';
 import { InteractionThroughputFilter } from '../filtering/interaction-throughput-filter';
 import { Filter } from '../filtering';

commit c682bda899e92e169149fac6122b4d4255fc918e

Fix pages broken by 40ae3c02fa96

src/app/annotation-sub-table/annotation-sub-table.component.ts
@@ -121,7 +121,7 @@ export class AnnotationSubTableComponent implements OnInit, OnChanges {
     this.tableViewChangeEmitter.emit(this.currentViewState);
   }
 
-  trackByTermId(item: any) {
+  trackByTermId(_: number, item: any) {
     return item.term.termid;
   }
 