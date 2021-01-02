pombase/website

commit 40ae3c02fa96ef1291946ba39f7c3b9ed8030d1a

Fix more Typescript warnings

src/app/query.service.ts
@@ -18,12 +18,12 @@ type SequenceOptions = 'protein' | 'none' | {
 };
 
 export class GAFOptions {
-  constructor(private aspects: Array<string>) { }
+  constructor(_aspects: Array<string>) { }
 }
 
 export class QueryOutputOptions {
-  constructor(private field_names: Array<string>,
-              private flags: Array<string>,
+  constructor(_field_names: Array<string>,
+              _flags: Array<string>,
               private sequence?: SequenceOptions,
               private gaf_options?: GAFOptions) {
     if (!this.sequence) {

src/app/annotation-sub-table/annotation-sub-table.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit, Input, Output, OnChanges, EventEmitter } from '@angular/core';
-import { TermAnnotation, Annotation, ReferenceShort } from '../pombase-api.service';
+import { TermAnnotation, ReferenceShort } from '../pombase-api.service';
 
 import { getAnnotationTableConfig, AnnotationTableConfig, AnnotationType,
          FilterConfig, SplitByParentsConfig, AnnotationExternalLinkConfig,
          getAppConfig,
-         AppConfig,
-         LinkDisplay} from '../config';
+         AppConfig} from '../config';
 import { AnnotationTable } from '../pombase-api.service';
 import { TableViewState } from '../pombase-types';
-import { TermShort, TermXref } from '../pombase-query';
+import { TermShort } from '../pombase-query';
 import { Filter } from '../filtering';
 
 @Component({
@@ -122,7 +121,7 @@ export class AnnotationSubTableComponent implements OnInit, OnChanges {
     this.tableViewChangeEmitter.emit(this.currentViewState);
   }
 
-  trackByTermId(index: number, item: any) {
+  trackByTermId(item: any) {
     return item.term.termid;
   }
 

src/app/app.component.ts
@@ -6,8 +6,6 @@ import { Title } from '@angular/platform-browser';
 
 import { filter, map, mergeMap } from 'rxjs/operators';
 
-import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
-
 import { getAppConfig } from './config';
 
 @Component({

src/app/display-sequence.ts
@@ -74,7 +74,7 @@ export class DisplaySequence {
   }
 
   private constructor(private readonly lineLength: number,
-                      private readonly displayParts: Array<RawSequencePart>) {
+                      public readonly displayParts: Array<RawSequencePart>) {
     let exonIndex = 1;
     displayParts.map(part => {
       if (part.partType === 'exon') {

src/app/filtering.ts
@@ -20,7 +20,7 @@ export class FilterCombiner<T> implements Filter<T> {
       filterResult = filter.filter(filterResult[0]);
     }
 
-    let [filteredTable, _, filteredAnnotationCount] = filterResult;
+    let [filteredTable, , filteredAnnotationCount] = filterResult;
 
     return [filteredTable, totalAnnotationCount, filteredAnnotationCount];
   }

src/app/filtering/annotation-extension-filter.ts
@@ -1,4 +1,4 @@
-import { AnnotationTable, Annotation } from '../pombase-api.service';
+import { AnnotationTable } from '../pombase-api.service';
 import { TermShort } from '../pombase-query';
 import { Filter } from '../filtering';
 

src/app/gene-external-references/gene-external-references.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnChanges, Input } from '@angular/core';
 
-import { getAppConfig, ExternalGeneReference } from '../config';
-
 import { GeneDetails } from '../pombase-api.service';
 
 @Component({

src/app/interaction-table-filters/interaction-table-filters.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, Input, Output, OnChanges,
-         EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { InteractionAnnotation, InteractionTable } from '../pombase-api.service';
 import { FilterConfig } from '../config';
 import { InteractionFilter, FilterCombiner } from '../filtering';
-import { TableViewState } from '../pombase-types';
 
 @Component({
   selector: 'app-interaction-table-filters',

commit 29159fb00eb1f60d91ec9c708ba717fa3ea1867d

Fix server error caused by missing field
Broken in 40ae3c02fa96

From Rocket:
  Error: Couldn't parse JSON body: Error("missing field `field_names`", line: 1, column: 249)

src/app/query.service.ts
@@ -22,8 +22,8 @@ export class GAFOptions {
 }
 
 export class QueryOutputOptions {
-  constructor(_field_names: Array<string>,
-              _flags: Array<string>,
+  constructor(public field_names: Array<string>,
+              public flags: Array<string>,
               private sequence?: SequenceOptions,
               private gaf_options?: GAFOptions) {
     if (!this.sequence) {