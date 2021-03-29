fa3964ae7b5750aba8894add34436775a03fa2ae
Kim Rutherford <kmr@aska.gen.nz>
2021-03-29 20:52:28 +1300

    Add a scale bar on track view diagram
    Refs #143

--- src/app/track-view/track-view.component.ts
+++ src/app/track-view/track-view.component.ts
@@ -31,6 +31,9 @@ export class TrackViewComponent implements OnInit, OnChanges {
 
   totalHeight = 0;
 
+  scaleMarkMinSpacing = 100;
+  scaleMarkPositions: Array<number> = [];
+
   constructor() { }
 
   featureHeight(): number {
@@ -42,11 +45,15 @@ export class TrackViewComponent implements OnInit, OnChanges {
   }
 
   trackLabelWidth(): number {
-    return 80;
+    return 100;
   }
 
   trackWidth(): number {
     return 600;
+  }
+
+  scaleBarY(): number {
+    return this.totalHeight + 10
   }
 
   widthScaleFactor(): number {
@@ -54,7 +61,7 @@ export class TrackViewComponent implements OnInit, OnChanges {
   }
 
   viewBox(): string {
-    return `-5 -5 ${this.trackWidth()+this.trackLabelWidth()+80} ${this.totalHeight+10}`;
+    return `-5 -5 ${this.trackWidth()+this.trackLabelWidth()+80} ${this.totalHeight+60}`;
   }
 
   setHighlighted(id: string) {
@@ -76,7 +83,8 @@ export class TrackViewComponent implements OnInit, OnChanges {
     this.totalHeight = 0;
     this.displayTracks = [];
     for (const track of this.trackViewData) {
-      this.totalHeight += track.features.length * (this.featureHeight() + this.featureLabelHeight());
+      this.totalHeight +=
+        track.features.length * (this.featureHeight() + this.featureLabelHeight());
 
       for (let i = 0; i < track.features.length; i++) {
         let label = null;
@@ -87,6 +95,24 @@ export class TrackViewComponent implements OnInit, OnChanges {
         const displayTrack = new DisplayTrack(label, track.dbName, feature);
         this.displayTracks.push(displayTrack);
       }
+    }
+
+    let scaleMarkSpacing = 10;
+    if (scaleMarkSpacing * this.widthScaleFactor() < this.scaleMarkMinSpacing) {
+      const scaledSpacing = this.scaleMarkMinSpacing / this.widthScaleFactor();
+      if (scaledSpacing > 100) {
+        scaleMarkSpacing = Math.round(scaledSpacing / 100) * 100;
+      } else {
+        scaleMarkSpacing = Math.round(scaledSpacing / 10) * 10;
+      }
+    }
+
+    this.scaleMarkPositions = [];
+
+    for (let pos = scaleMarkSpacing;
+         pos < this.protein.number_of_residues;
+         pos += scaleMarkSpacing) {
+      this.scaleMarkPositions.push(pos);
     }
   }
 }
