fef344d1e3f2ab94c81e2df80a5bd42391bd097a
Kim Rutherford <kmr@aska.gen.nz>
2021-05-02 00:05:09 +1200

    Allow alternative index.html files

--- etc/update_vars.sh
+++ etc/update_vars.sh
@@ -2,7 +2,8 @@
 
 analytics_id=$1
 app_deploy_config="$2"
-index=$3
+input_index_file=$3
+output_index_file=$4
 
-perl -pne "s/'<<APP_DEPLOY_CONFIG>>'/$app_deploy_config/; s/<<GOOGLE_ANALYTICS_ID>>/$analytics_id/;" $index > /tmp/index.tmp &&
-  mv /tmp/index.tmp $index
+perl -pne "s/'<<APP_DEPLOY_CONFIG>>'/$app_deploy_config/; s/<<GOOGLE_ANALYTICS_ID>>/$analytics_id/;" $input_index_file > /tmp/index.tmp &&
+  mv /tmp/index.tmp $output_index_file
