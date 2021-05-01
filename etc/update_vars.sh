1a38e16fc03a93afa9a8565a1be7878c11074177
Kim Rutherford <kmr@aska.gen.nz>
2021-05-02 01:47:16 +1200

    Revert "Allow alternative index.html files"
    This reverts commit fef344d1e3f2ab94c81e2df80a5bd42391bd097a.

--- etc/update_vars.sh
+++ etc/update_vars.sh
@@ -2,8 +2,7 @@
 
 analytics_id=$1
 app_deploy_config="$2"
-input_index_file=$3
-output_index_file=$4
+index=$3
 
-perl -pne "s/'<<APP_DEPLOY_CONFIG>>'/$app_deploy_config/; s/<<GOOGLE_ANALYTICS_ID>>/$analytics_id/;" $input_index_file > /tmp/index.tmp &&
-  mv /tmp/index.tmp $output_index_file
+perl -pne "s/'<<APP_DEPLOY_CONFIG>>'/$app_deploy_config/; s/<<GOOGLE_ANALYTICS_ID>>/$analytics_id/;" $index > /tmp/index.tmp &&
+  mv /tmp/index.tmp $index
