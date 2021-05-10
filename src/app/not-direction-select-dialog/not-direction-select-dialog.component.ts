6b661a480dbe6936ad7a5f31f2b34abbf5695d5d
Kim Rutherford <kmr@aska.gen.nz>
2021-05-10 16:08:15 +1200

    Fix Not direction dialog in Angular release mode
    Refs pombase/website#463

--- src/app/not-direction-select-dialog/not-direction-select-dialog.component.ts
+++ src/app/not-direction-select-dialog/not-direction-select-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
+import { Subject } from "rxjs";
+
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { GeneQuery } from '../pombase-query';
 
