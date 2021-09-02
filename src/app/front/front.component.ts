0113bb7de385d0e686f7f4131594249c2e1700d4
Kim Rutherford <kmr@aska.gen.nz>
2021-09-03 09:53:17 +1200

    Fix missing Twitter feed on front page

--- src/app/front/front.component.ts
+++ src/app/front/front.component.ts
@@ -36,7 +36,7 @@ export class FrontComponent implements OnInit {
   siteName = getAppConfig().site_name;
 
   examplePages = getAppConfig().example_pages;
-  twitterAccount = getAppConfig().twitter_account;
+  twitterAccount = getAppConfig().social_media?.twitter;
   hasDiseaseAnnotation = getAppConfig().has_disease_annotation;
   showNewsItems = getAppConfig().news_on_front_page;
   welcomeMessage = getAppConfig().welcome_message;
