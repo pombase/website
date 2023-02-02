8860f79e4d1dc8211a0a2f14671002bf99bbc403
Kim Rutherford <kmr@aska.gen.nz>
2023-02-02 19:27:43 +1300

    Convert PMIDs in gene history refs column to links
    Refs pombase/website#1993

--- src/app/gene-history-table/gene-history-table.component.ts
+++ src/app/gene-history-table/gene-history-table.component.ts
@@ -1,17 +1,60 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { GeneDetails } from '../pombase-api.service';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { GeneDetails, GeneHistoryEntry } from '../pombase-api.service';
+
+type RefOrText = {
+  text?: string;
+  referenceUniquename?: string;
+}
+
+interface ProcessedHistoryEntry extends GeneHistoryEntry {
+  processedReferences: Array<Array<RefOrText>>;
+}
 
 @Component({
   selector: 'app-gene-history-table',
   templateUrl: './gene-history-table.component.html',
   styleUrls: ['./gene-history-table.component.css']
 })
-export class GeneHistoryTableComponent implements OnInit {
+export class GeneHistoryTableComponent implements OnInit, OnChanges {
   @Input() geneDetails: GeneDetails;
+
+  processedHistory: Array<ProcessedHistoryEntry> = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(): void {
+    this.processedHistory = [];
+
+    for (const historyEntry of this.geneDetails.gene_history) {
+      let processedHistoryEntry = Object.assign({}, historyEntry) as ProcessedHistoryEntry;
+      processedHistoryEntry.processedReferences = [];
+      for (const ref of processedHistoryEntry.references) {
+        let refParts = [];
+        for (const part of ref.split(/(PMID:\d\d\d+)/)) {
+          let text = undefined;
+          let referenceUniquename = undefined;
+
+          if (part.match(/^(PMID:\d\d\d+)$/)) {
+            referenceUniquename = part;
+          } else {
+            text = part;
+          }
+
+          const processedRef = {
+            text,
+            referenceUniquename,
+          } as RefOrText;
+
+          refParts.push(processedRef);
+        }
+
+        processedHistoryEntry.processedReferences.push(refParts);
+      }
+
+      this.processedHistory.push(processedHistoryEntry);
+    }
+  }
 }
