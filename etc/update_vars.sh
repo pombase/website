c78372a2e272b5a9a1ab21920d35d11af3fe5a8f
Kim Rutherford <kmr@aska.gen.nz>
2023-03-06 19:01:22 +1300

    Add Google Tag Manager ID
    Refs pombase/website#1870

--- etc/update_vars.sh
+++ etc/update_vars.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -
 
-analytics_id=$1
-app_deploy_config="$2"
+analytics_id="$1"
+google_tag_manager_id="$2"
+app_deploy_config="$3"
 index=$3
 
-perl -pne "s/'<<APP_DEPLOY_CONFIG>>'/$app_deploy_config/; s/<<GOOGLE_ANALYTICS_ID>>/$analytics_id/;" $index > /tmp/index.tmp &&
+perl -pne "s/'<<APP_DEPLOY_CONFIG>>'/$app_deploy_config/; s/<<GOOGLE_ANALYTICS_ID>>/$analytics_id/; s/<<GOOGLE_TAG_MANAGER_ID>>/$google_tag_manager_id/;" $index > /tmp/index.tmp &&
   mv /tmp/index.tmp $index
