8dd4a60991929e1628cfe02c4bf5d4f1e0c7d6bc
Kim Rutherford <kmr@aska.gen.nz>
2023-03-06 20:18:55 +1300

    Fix broken update_vars.sh
    Refs pombase/website#1870

--- etc/update_vars.sh
+++ etc/update_vars.sh
@@ -3,7 +3,7 @@
 analytics_id="$1"
 google_tag_manager_id="$2"
 app_deploy_config="$3"
-index=$3
+index=$4
 
 perl -pne "s/'<<APP_DEPLOY_CONFIG>>'/$app_deploy_config/; s/<<GOOGLE_ANALYTICS_ID>>/$analytics_id/; s/<<GOOGLE_TAG_MANAGER_ID>>/$google_tag_manager_id/;" $index > /tmp/index.tmp &&
   mv /tmp/index.tmp $index
