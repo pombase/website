cd6eb3d80119f577cf198c0eaa5ae6ccf852223f
Kim Rutherford <kmr@aska.gen.nz>
2024-08-07 13:22:55 +1200

    Allow ";" as a gene ID separator in upload tool
    Refs pombase/website#2206

--- src/app/gene-list-lookup/gene-list-lookup.component.ts
+++ src/app/gene-list-lookup/gene-list-lookup.component.ts
@@ -33,7 +33,7 @@ export class GeneListLookupComponent implements OnInit {
 
   filteredIds(): Array<string> {
     let seen: { [key: string]: boolean } = {};
-    return this.inputText.trim().split(/[,\s\u200B]+/)
+    return this.inputText.trim().split(/[,\s;\u200B]+/)
       .filter(id => {
         id = id.trim();
         if (id.length === 0) {
