16cb67cf3a04c58484962f8f3a8b9ba52d0bcd2e
Kim Rutherford <kmr@aska.gen.nz>
2025-01-31 00:30:54 +1300

    Don't add tracks in JBrowse V2 init. script
    We now generate the configuration file from the CSV metadata table.
    
    Refs pombase/website#2038

--- etc/setup_jbrowse2_in_container.sh
+++ etc/setup_jbrowse2_in_container.sh
@@ -1,12 +1,5 @@
 #!/bin/sh -
 
-# Create a JBrowse2 instance instead the website container and populate tracks
+# Create a JBrowse2 instance inside the website container
 
 jbrowse create jbrowse2
-jbrowse add-assembly https://www.pombase.org/internal_datasets/bgzip_chromosomes/Schizosaccharomyces_pombe_all_chromosomes.fa.gz --name pombe_v1 --type bgzipFasta --out jbrowse2
-
-cd jbrowse2
-
-jbrowse add-track /pombase/gff/Schizosaccharomyces_pombe_all_chromosomes_forward_strand.gff3 --load copy --name "Forward strand" --category "Genes" --config '{"renderer": {"type": "SvgFeatureRenderer"}}'
-jbrowse add-track /pombase/gff/Schizosaccharomyces_pombe_all_chromosomes_reverse_strand.gff3 --load copy --name "Reverse strand" --category "Genes" --config '{"renderer": {"type": "SvgFeatureRenderer"}}'
-
