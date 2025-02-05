4900c945c97243b49d9a1bb31427ba3773b55024
Kim Rutherford <kmr@aska.gen.nz>
2025-02-05 19:14:45 +1300

    Color genes by characterisation_status in JBrowse2
    Also fix code for making RNA feature and labels smaller.
    
    Refs pombase/website#2038

--- etc/PomBasePlugin.js
+++ etc/PomBasePlugin.js
@@ -4,32 +4,53 @@
   class PomBasePlugin {
     install() {}
     configure(pluginManager) {
+      const geneValues = (gene) => {
+        let so_term_name = gene.get('so_term_name');
+        let characterisation_status = gene.get('characterisation_status');
+        if (so_term_name === 'protein_coding_gene') {
+          return ['protein_coding_gene', characterisation_status];
+        } else if (so_term_name.includes("RNA")) {
+          return ['RNA', characterisation_status];
+        }
+      };
       const getBaseType = (feature) => {
+        if (feature.get('type') === 'gene') {
+          return geneValues(feature);
+        }
         if (feature.get('type') === 'CDS') {
           let transcript = feature.parent();
           if (transcript !== undefined) {
             let gene = transcript.parent();
             if (gene !== undefined) {
-              let so_term_name = gene.get('so_term_name');
-              if (so_term_name === 'protein_coding_gene') {
-                return 'protein_coding_gene';
-              } else if (so_term_name.includes("RNA")) {
-                return 'RNA';
-              }
+              return geneValues(gene);
             }
           }
-          return 'other';
+          return ['other', undefined];
         }
 
-        return 'misc';
+        return ['misc', undefined];
       };
       pluginManager.jexl.addFunction('featureColor', feature => {
-        const baseType = getBaseType(feature);
+        const [baseType, characterisationStatus] = getBaseType(feature);
         if (baseType === 'protein_coding_gene') {
-          return 'goldenrod';
+          const colMap = {
+            'biological role published': '#ff0000',
+            'transposon': '#0000ff',
+            'dubious': '#ff00ff',
+            'biological role inferred': '#e8ef00',
+            'Schizosaccharomyces pombe specific protein, uncharacterized': '#98fb98',
+            'conserved unknown': '#ffa500',
+            'Schizosaccharomyces specific protein, uncharacterized': '#ffc8c8',
+            'pseudogene': '#aaaaaa',
+          };
+          if (characterisationStatus) {
+            return colMap[characterisationStatus] || 'goldenrod';
+          } else {
+            return 'goldenrod';
+          }
         }
         if (baseType === 'RNA') {
-          return '#f44';
+          return '#aaa';
         }
         if (baseType === 'misc') {
           return '#4fc2e7';
@@ -38,20 +59,21 @@
       });
 
       pluginManager.jexl.addFunction('featureHeight', feature => {
-        const baseType = getBaseType(feature);
+        const [baseType, _] = getBaseType(feature);
         if (baseType === 'protein_coding_gene') {
-          return 10;
+          return 12;
         } else {
-          return 7;
+          return 8;
         }
       });
 
       pluginManager.jexl.addFunction('featureLabelFontSize', feature => {
-        const baseType = getBaseType(feature);
+        const [baseType, _] = getBaseType(feature);
+        console.log(baseType + ' ' + feature.get('type'));
         if (baseType === 'protein_coding_gene') {
           return 12;
         } else {
-          return 10;
+          return 9;
         }
       });
     }
