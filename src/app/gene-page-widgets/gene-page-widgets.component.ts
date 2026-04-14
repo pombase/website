f117c9cd6418ac0d11dbd96149b930b9bd8bb9ea
Kim Rutherford <kmr@aska.gen.nz>
2026-04-14 21:24:46 +1200

    Revert "Remove RNA 2D structure due to...API fail"
    This reverts commit 18dbe81761666d594ac24c3b48b7fe2e32af28a7.

--- src/app/gene-page-widgets/gene-page-widgets.component.ts
+++ src/app/gene-page-widgets/gene-page-widgets.component.ts
@@ -131,10 +131,9 @@ export class GenePageWidgetsComponent implements OnInit, OnChanges {
   }
 
   hasRna2dStructure(): boolean {
-    return false;
-//    return !!this.geneDetails.rnacentral_2d_structure_id &&
-//      ['tRNA gene', 'snRNA gene', 'snoRNA gene', 'rRNA gene']
-//        .includes(this.geneDetails.feature_type);
+    return !!this.geneDetails.rnacentral_2d_structure_id &&
+      ['tRNA gene', 'snRNA gene', 'snoRNA gene', 'rRNA gene']
+        .includes(this.geneDetails.feature_type);
   }
 
   showProteinFeatures(): boolean {
